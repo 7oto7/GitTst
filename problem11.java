762bcc054601be22c81509b2af21925056e775e8
S <s>
2023-10-12 17:47:59 +0400

    {"types":"C","runs":18}

--- problem11.java
+++ problem11.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 
+import acm.graphics.GLine;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
@@ -8,6 +9,7 @@ public class problem11 extends GraphicsProgram {
 	private static final double HOUSE_HEIGHT = 200;
 	private static final double DOOR_WIDTH = 200;
 	private static final double DOOR_HEIGHT = 200;
+	private static final double ROOF_HEIGHT = 30;
 	public void run() {
 		
 		DrawHouse();
@@ -16,12 +18,22 @@ public class problem11 extends GraphicsProgram {
 	
 	private void DrawHouse() {
 		drawWall();
-	//	drawRoof();
+		drawRoof();
 	//	drawDoor();
 	//	drawWindows();
 		
 	}
 	
+	private void drawRoof() {
+		double x1 = getWidth() / 2 - HOUSE_WIDTH / 2;
+		double y1 = getHeight() - HOUSE_HEIGHT;
+		double x2 = x1 + HOUSE_WIDTH / 2;
+		double y2 = y1 - ROOF_HEIGHT;;
+		GLine leftRoof = new GLine(x1, y1, x2, y2);
+		add(leftRoof);
+		
+	}
+
 	private void drawWall() {
 		GRect wall = new GRect(HOUSE_WIDTH, HOUSE_HEIGHT);
 		int x = (int) (getWidth() / 2 - HOUSE_WIDTH / 2); //dakastva mrgvaldeba yoveltvis dabla, 3.1=3, 3.9=3;
