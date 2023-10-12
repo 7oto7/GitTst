8974ad9f9492ff22b697069479ba8b88f4063944
S <s>
2023-10-12 17:43:09 +0400

    {"types":"C","runs":17}

--- problem11.java
+++ problem11.java
@@ -25,7 +25,7 @@ public class problem11 extends GraphicsProgram {
 	private void drawWall() {
 		GRect wall = new GRect(HOUSE_WIDTH, HOUSE_HEIGHT);
 		int x = (int) (getWidth() / 2 - HOUSE_WIDTH / 2); //dakastva mrgvaldeba yoveltvis dabla, 3.1=3, 3.9=3;
-		int y = getHeight() - getHeight();
+		double y = getHeight() - HOUSE_HEIGHT;
 		add(wall, x, y);		
 	}
 }
