f5f395ade29dab9cca1ac6554414b6cf608dc131
S <s>
2023-10-12 17:04:09 +0400

    {"types":"C","runs":1}

--- problem11.java
+++ problem11.java
@@ -1,4 +1,9 @@
 
-public class problem11 {
+import acm.graphics.GLabel;
+import acm.program.*;
 
+public class problem11 extends GraphicsProgram {	
+	public void run() {
+		GRect rect = new GRect(200, 200);
+	}
 }
