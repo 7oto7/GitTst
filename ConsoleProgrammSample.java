2dad2045d43a5c2d36d7c06ece81d7a715b0d364
S <s>
2023-10-12 18:02:15 +0400

    {"types":"C","runs":24}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -9,11 +9,20 @@ import acm.program.ConsoleProgram;
 
 public class ConsoleProgrammSample extends ConsoleProgram {
 	public void run() {
-//		int kids = readInt("enter number of your kids: ");
-		
+//		int kids = readInt("enter number of your kids: ");		
 //		println("your kids number: " + kids);
 		
-		boolean isAlive = readBoolean("is alive?");
-		println(isAlive);
+//		boolean isAlive = readBoolean("is alive?");
+//		println(isAlive);
+		
+		String NUM1 = readLine("enter string: ");
+		println(NUM1);
+		
+		String NUM2 = readLine("enter string: ");
+		println(NUM2);
+		
+		println(NUM1 + NUM2);
+		
+		
 	}
 }
