7a38a028fae31658ab89b46415ed8f693bacb60d
S <s>
2023-10-12 17:17:47 +0400

    {"types":"C","runs":9}

--- problem12.java
+++ problem12.java
@@ -1,4 +1,21 @@
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
 
-public class problem12 {
-
+public class problem12 extends GraphicsProgram {
+	public void run(){
+		int a = 5;
+		a = a + 1;
+		
+		if(2 == 2){
+			double b = 3;
+			b = 4;
+			b = a + 3;
+			
+			GLabel label = new GLabel ("a :" + a, 20, 20);
+			add(label);
+		}
+		
+		b = b + 1;
+		
+	}
 }
