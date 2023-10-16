d34132e22ce01ca79dea9c05d3d3cd6be384121f
S <s>
2023-10-17 02:46:21 +0400

    {"types":"C","runs":12}

--- problem11.java
+++ problem11.java
@@ -77,7 +77,7 @@ public class problem11 extends GraphicsProgram {
 		int x = (int) (getWidth() / 2 - HOUSE_WIDTH / 2); //dakastva mrgvaldeba yoveltvis dabla, 3.1=3, 3.9=3;
 		double y = getHeight() - HOUSE_HEIGHT;
 		wall.setFilled(true);
-		wall.setFillColor(Color.red);
+		wall.setColor(Color.red);
 		add(wall, x, y);		
 	}
 }
