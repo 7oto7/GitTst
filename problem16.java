c9da33aa9716d560f41a5968645bda5c367a787b
S <s>
2023-10-17 03:44:57 +0400

    {"types":"C","runs":51}

--- problem16.java
+++ problem16.java
@@ -1,4 +1,12 @@
 
-public class problem16 {
+import acm.program.ConsoleProgram;
 
+public class problem16 extends ConsoleProgram {
+	public void run() {
+		double firstNum = (int) (readInt("First number is: "));
+		double secondNum = (int) (readInt("Second number is: "));
+		double thirdNum = (int) (readInt("Third number is : "));
+		double average = 1 / (1 / firstNum + 1 / secondNum + 1 / thirdNum);
+		println("Average is: " + average);		
+	}
 }
