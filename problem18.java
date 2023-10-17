eb8020efe2c17594caaf3f0077db2b622547e8b7
S <s>
2023-10-17 13:20:21 +0400

    {"types":"C","runs":21}

--- problem18.java
+++ problem18.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
 
-public class problem18 {
+public class problem18 extends ConsoleProgram {
+	public void run() {
 
+		//1 dan n mde ricxvebs 1 aklia da unda gamovataninot
+		//jami davtvalot da mtlians gamovaklot
+		//n davitvalot while dan
+	}
 }
