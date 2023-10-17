f60c7e4662c0e851a34608db3f12b65d41e7bc06
S <s>
2023-10-17 13:07:01 +0400

    {"types":"A","runs":19}

---
+++ problem19.java
@@ -0,0 +1,11 @@
+import acm.program.ConsoleProgram;
+
+public class problem19 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("number is: ");
+		for(int i = 0; i < n -1; i++){
+			println("hello");
+		}
+		
+	}
+}
