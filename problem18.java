026ead0242a8bf134741c216992dc59c8fb5ba91
S <s>
2023-10-19 01:45:27 +0400

    {"types":"C","runs":1}

--- problem18.java
+++ problem18.java
@@ -1,7 +1,18 @@
 import acm.program.ConsoleProgram;
 
 public class problem18 extends ConsoleProgram {
+	private static final int SENTINEL = 0;
+	
 	public void run() {
+		int num = readInt("number: ");
+		int n = 0;
+		int total = 0;
+		while(num != SENTINEL){
+			n++;
+			total = num + num;
+			num = readInt("number: ");
+		}
+		println(n);
 
 		//1 dan n mde ricxvebs 1 aklia da unda gamovataninot
 		//jami davtvalot da mtlians gamovaklot
