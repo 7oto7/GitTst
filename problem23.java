8531b9612ec1c29096eec24a6f16ce975ebc4a08
S <s>
2023-10-19 18:50:04 +0400

    {"types":"C","runs":8}

--- problem23.java
+++ problem23.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 
 public class problem23 extends ConsoleProgram {
 	public void runumber() {
-		int number = readInt("Enumberter numberumber: ");
+		int n = readInt("Enumberter numberumber: ");
 		int sum = ((1 + number) / 2) * number;
 		println(sum);
 	}
