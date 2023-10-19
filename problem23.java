444e494f5f4746a8f2433a0e821bcff385cb7bf8
S <s>
2023-10-19 18:46:08 +0400

    {"types":"C","runs":5}

--- problem23.java
+++ problem23.java
@@ -4,7 +4,7 @@ import acm.program.ConsoleProgram;
 public class problem23 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter number: ");
-		int sum = ((2 + n) / 2) * (n + 1);
+		int sum = ((1 + n) / 2) * n;
 		println(sum);
 	}
 }
