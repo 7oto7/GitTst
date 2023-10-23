35256165a1db7b5906671ba53e49b8656f4121fe
S <s>
2023-10-23 22:27:43 +0400

    {"types":"C","runs":17}

--- problem27.java
+++ problem27.java
@@ -1,4 +1,10 @@
+//მომხმარებელს შეყავს n და m დაბეჭდეთ n^m
+import acm.program.ConsoleProgram;
 
-public class problem27 {
-
+public class problem27 extends ConsoleProgram{
+	public void run(){
+		int n = readInt("Enter n: ");
+		int m = readInt("Enter m: ");
+		println(n ^ m);
+	}
 }
