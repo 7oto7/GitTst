93bc335b71a44499a494724378080fb419075012
S <s>
2023-10-26 17:38:27 +0400

    {"types":"C","runs":1}

--- problem29.java
+++ problem29.java
@@ -4,18 +4,25 @@ import acm.program.ConsoleProgram;
 
 public class problem29 extends ConsoleProgram {
 	public void run(){
+		int fibonacci = 0;
 		int a = 0;
 		int b = 1;
-		int fibonacci;
 		int n = readInt("Enter number: ");
-		if(n == 0){
-			fibonacci = a;
-		} else if(n == 1){
-			fibonacci = b;
+		if(n == 1){
+			fibonacci = 0;
+		} else if(n == 2 ){
+			fibonacci = 1;
 		} else {
 			for(int i = 3; i <= n; i++){
-				
-			}
+				if( i % 2 == 1){
+					a = b + a;
+					fibonacci = a;
+				} else {
+					b = a + b;
+					fibonacci = b;
+				}
+			}		
 		}
+		println(fibonacci);
 	}
 }
