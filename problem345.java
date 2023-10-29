2cae82d0e5a4b4c137e4e1cdd31696aae4c7a6c5
S <s>
2023-10-29 20:11:58 +0400

    {"types":"C","runs":1}

--- problem345.java
+++ problem345.java
@@ -1,4 +1,29 @@
 
-public class problem345 {
+import acm.program.*;
 
+public class problem345 extends ConsoleProgram {
+
+	public void run() {
+
+		while (true) {
+			int n = readInt("Enter integer: ");
+			if (n == -1)
+				break;
+			println(luckyNumber(n) ? "Is Lucky" : "Is not Lucky");
+		}
+	}
+	
+	
+	// Lucky number method
+	private boolean luckyNumber(int n) {
+
+		while (n != 0) {
+
+			int reminder = n % 10;
+			if (reminder != 4 && reminder != 7)
+				return false;
+			n /= 10;
+		}
+		return true;
+	}
 }
