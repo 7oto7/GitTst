58e0848d44382e58e8ba786ff5aa35fe189d1fa6
S <s>
2023-11-02 17:26:16 +0400

    {"types":"C","runs":13}

--- problem34.java
+++ problem34.java
@@ -1,12 +1,29 @@
+//კანვასზე დახატეთ 1000 შემთხვევითი რადიუსის მქონე შემთხვევითი ფერით გაფერადებული
+//წრე, შემთხვევით კოორდინატებზე.
+
+import java.awt.Color;
+
+import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class problem34 extends GraphicsProgram{
+	private static final int MIN_RADIUS = 10;
+	private static final int MAX_RADIUS = 20;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	public void run() {
-		boolean a = rgen.nextBoolean();
-		int b = rgen.nextInt(9);
-		println(b);
-		println(a);
+		
+	}
+	private void addRandomCircle(){
+		int radius = rgen.nextInt(MIN_RADIUS, MAX_RADIUS);
+		double x = rgen.nextDouble(0, getWidth() - 2 * radius);
+		double y = rgen.nextDouble(0, getHeight() - 2 * radius);
+		Color color = rgen.nextColor();
+		
+		GOval circle = new GOval(2 * radius, 2 * radius);
+		circle.setFilled(true);
+		circle.setColor(color);
+		
+		add(circle, x, y);
 	}
 }
