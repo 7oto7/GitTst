39fb41e47e98d1b2faf5e66e86153838759426ba
S <s>
2023-11-02 16:41:47 +0400

    {"types":"C","runs":2}

--- problem31.java
+++ problem31.java
@@ -3,6 +3,17 @@ import acm.program.ConsoleProgram;
 
 public class problem31 extends ConsoleProgram{
 	public void run(){
-		
+		int n = readInt("Enter number: ");
+		if(n % 2 == 0){
+			println(n + " is not prime");
+		} else {
+			for(int i = 3; i < n - 1; i += 2){
+				if(n % i == 0){
+				println(n + " is not prime");
+				} else {
+					println(n + " is prime");
+				}
+			}
+		}
 	}
 }
