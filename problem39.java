8bafe20cd147ac378963f0484041c143f942ff33
S <s>
2023-11-07 18:01:53 +0400

    {"types":"C","runs":5}

--- problem39.java
+++ problem39.java
@@ -1,3 +1,4 @@
+
 //დაწერეთ პროგრამა სადაც იქნება კანვასზე ბურთის მოძრაობა მარცხენა კედლის შუა
 //წერტილიდან მარჯვენა კედლამდე.
 import java.awt.Color;
@@ -7,18 +8,24 @@ import acm.program.GraphicsProgram;
 
 public class problem39 extends GraphicsProgram {
 	private static final int SPEED = 5;
+	private static final int PAUSE_MS = 20;
+	private static final int RADIUS = 20;
 
 	public void run() {
-		makeCircle(20, 20);
-		
+		GOval circle = makeCircle();
+		while (circle.getX() + 2 * RADIUS + SPEED <= getWidth()) {
+			circle.move(SPEED, 0);
+			pause(PAUSE_MS);
+		}
 	}
 
-	private void makeCircle(int i, int j) {
-		GOval oval = new GOval(i * 2, j * 2);
+	private GOval makeCircle() {
+		GOval oval = new GOval(RADIUS * 2, RADIUS * 2);
 		int x = 0;
-		int y = getHeight() / 2 - j;
+		int y = getHeight() / 2 - RADIUS;
 		oval.setFilled(true);
 		oval.setFillColor(Color.RED);
 		add(oval, x, y);
+		return oval;
 	}
 }
