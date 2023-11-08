a3be938477cde5e53a42e5f8cb9db06814e28ec3
S <s>
2023-11-08 19:38:39 +0400

    {"types":"C","runs":8}

--- problem33.java
+++ problem33.java
@@ -8,14 +8,14 @@ import acm.util.RandomGenerator;
 //რაიდიუსიც შემთხვევით აირჩეით შეზღუდული რეინჯიდან.
 public class problem33 extends GraphicsProgram{
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private static final int MIN_RADIUS = 20;
-	private static final int MAX_RADIUS = 1000;
+	private static final int MIN_RADIUS = 0;
+//	private static final int MAX_RADIUS = getHeight() / 2;
 	
 	public void run(){
 		addRandomCircle();
 	}
 	private void addRandomCircle() {
-		int radius = rgen.nextInt(MIN_RADIUS, MAX_RADIUS);
+		int radius = rgen.nextInt(MIN_RADIUS, getHeight() / 2);
 		Color color = rgen.nextColor();
 		double x = rgen.nextDouble(0, getWidth() - 2 * radius);
 		double y = rgen.nextDouble(0, getHeight() - 2 * radius);
