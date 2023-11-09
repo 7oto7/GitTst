ca92d45ecb572d9483f78a1cd9ed51bb80fe013e
S <s>
2023-11-09 17:16:48 +0400

    {"types":"C","runs":6}

--- tst.java
+++ tst.java
@@ -26,7 +26,7 @@ public class tst extends ConsoleProgram {
 					n++;
 				}
 			}
-			if (n == 2) {
+			if (n == 3) {
 				println("You lose");
 			}
 			println("Roulette result is: " + rouletteNumber);
