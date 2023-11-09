311fcac2d7fb02a53310beec71e411e3b0fb1449
S <s>
2023-11-09 17:44:11 +0400

    {"types":"C","runs":8}

--- bonus13_1.java
+++ bonus13_1.java
@@ -1,4 +1,10 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class bonus13_1 {
-
+public class bonus13_1 extends GraphicsProgram {
+	public void run() {
+		GLine line = new GLine(0, getHeight() / 2, getWidth() / 2, getHeight() / 2);
+		add(line);
+		pause(20);
+	}
 }
--- tst.java
+++ tst.java
@@ -1,50 +1,31 @@
-import acm.program.ConsoleProgram;
-import acm.util.RandomGenerator;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 
-public class tst extends ConsoleProgram {
-	private static final int MIN_VALUE = 0;
-	private static final int MAX_VALUE = 14;
-	private RandomGenerator rgen = RandomGenerator.getInstance();
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
+public class tst extends GraphicsProgram{
+	//
+	private GRect rect;
+	private GRect rect2;
 	public void run() {
-		int betNumber = readBet();
-		int rouletteNumber = spinRoulette();
-		int n = 0;
-		while (true) {
-			for (int i = 0; i < 2; i++) {
-				if (betNumber == rouletteNumber) {
-					println("you won");
-					break;
-				} else if (betNumber != rouletteNumber) {
-					if (betNumber < rouletteNumber) {
-						println("try bigger number");
-					} else if (betNumber > rouletteNumber) {
-						println("try less");
-					}
-					betNumber = readBet();
-					n++;
-				}
-			}
-			if (n == 2) {
-				println("You lose");
-			}
-			println("Roulette result is: " + rouletteNumber);
-			break;
-		}
+		rect = new GRect(100, 100);
+		rect.setFilled(true);
+		add(rect, 100, 100);
+		
+		rect2 = new GRect(100, 100);
+		rect.setFilled(true);
+		add(rect2, 200, 200);
+		
+		addMouseListeners();
+		addKeyListeners();
 	}
-
-	private int spinRoulette() {
-		int winningNum = rgen.nextInt(MIN_VALUE, MAX_VALUE);
-		// println("Roulette result is: " + winningNum);
-		return winningNum;
+	public void mouseClicked(MouseEvent e){
+		
 	}
-
-	private int readBet() {
-		while (true) {
-			int winningNum = readInt("Enter number from " + MIN_VALUE + " to " + MAX_VALUE + ": ");
-			if (winningNum >= MIN_VALUE && winningNum <= MAX_VALUE) {
-				return winningNum;
-			}
-		}
+	
+	@override
+	public void keyTyped(KeyEvent e){
+	//	label.setLabel("key typed: " + )
 	}
 }
