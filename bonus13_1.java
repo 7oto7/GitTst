82ceafc8630edc56e9ff4c5a87f3e5771c38837f
S <s>
2023-11-09 17:51:22 +0400

    {"types":"C","runs":11}

--- bonus13_1.java
+++ bonus13_1.java
@@ -2,10 +2,11 @@ import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 public class bonus13_1 extends GraphicsProgram {
+	private GLine line;
 	public void run() {
 		while(true){
 			//removeAll();
-			
+			remove(line);
 			GLine line = new GLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
 			add(line);
 			pause(20);
