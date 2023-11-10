06454effa36a35b1b3a64519bc583812a246ea91
S <s>
2023-11-10 14:23:14 +0400

    {"types":"C","runs":15}

--- problem39.java
+++ problem39.java
@@ -17,7 +17,8 @@ public class problem39 extends GraphicsProgram {
 		while (true) {
 			if(circle.getX() + 2 * RADIUS - SPEED <= getWidth()){
 				vx = SPEED;
-			}else if(circle.getX() + 2 * RADIUS - SPEED >= getWidth() || circle.getX() < 0){
+			}
+			if(circle.getX() + 2 * RADIUS - SPEED >= getWidth() || circle.getX() < 0){
 				vx = -vx;
 			}
 			circle.move(SPEED, 0);
