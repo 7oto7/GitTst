187dc080f9db4bcf3bce74ef578a1f0e5da698fc
S <s>
2023-11-11 15:10:09 +0400

    {"types":"C","runs":31}

--- dvd.java
+++ dvd.java
@@ -1,4 +1,40 @@
 
-public class dvd {
+import java.awt.Color;
 
+import acm.graphics.GLabel;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+
+public class dvd extends GraphicsProgram {
+	private static final int SPEED = 1;
+	private static final int PAUSE_MS = 2;
+	private static final int RADIUS = 20;
+
+	public void run() {
+		GLabel lable = myPanel();
+		int vx = SPEED;
+		int vy = SPEED;
+		while (true) {
+			if (lable.getX() + 2 * RADIUS <= getWidth() && lable.getY() + 2 * RADIUS <= getHeight()) {
+				lable.move(vx, vy);
+				pause(PAUSE_MS);
+			}
+			if (lable.getY() + 2 * RADIUS > getHeight() || lable.getY() < 0) {
+				vy = -vy;
+				lable.move(vx, vy);
+			}
+			if (lable.getX() + 2 * RADIUS > getWidth() || lable.getX() < 0) {
+				vx = -vx;
+				lable.move(vx, vy);
+				pause(PAUSE_MS);
+			}
+		}
+	}
+
+	private GLabel myPanel() {
+		GLabel lbl = new GLabel("D V D", 100, 100);
+		lbl.setFont("Helvetica-24");
+		add(lbl);
+	    return lbl;
+	}
 }
