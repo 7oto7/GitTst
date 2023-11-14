421508e5964179d0950dd86e82e4129554795a7a
S <s>
2023-11-14 16:44:48 +0400

    {"types":"C","runs":6}

--- practiseBreakOut.java
+++ practiseBreakOut.java
@@ -74,7 +74,7 @@ public class practiseBreakOut extends GraphicsProgram {
 
 	private void removeBricks() {
 		if (collider != null) {
-			if (collider == rect && ball.getY() + 2 * BALL_RADIUS == rect.getY()) {
+			if (collider == rect || ball.getY() + BALL_RADIUS == rect.getX() + PADDLE_WIDTH || ball.getX() + 2 * BALL_RADIUS == rect.getY()) {
 				vy = -vy;
 			} else {
 				remove(collider);
