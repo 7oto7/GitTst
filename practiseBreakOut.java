7f0159b70a2720e9bff193a9688d67b5ae3e3ad8
S <s>
2023-11-14 11:46:20 +0400

    {"types":"C","runs":27}

--- practiseBreakOut.java
+++ practiseBreakOut.java
@@ -52,7 +52,7 @@ public class practiseBreakOut extends GraphicsProgram {
 
 	private GRect rect;
 	private GOval ball;
-	private double vx;
+	private double vx = 1;
 	private double vy = 3;;
 	private GObject collider;
 	private int count;
@@ -124,6 +124,7 @@ public class practiseBreakOut extends GraphicsProgram {
 				if (rgen.nextBoolean(0.5)) {
 					vx = -vx;
 				}
+				vy = 3;
 				ballIsStopped = !ballIsStopped;
 			}
 			if(ballIsStopped == false){
