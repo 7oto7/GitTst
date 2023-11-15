7a657c11703417f1842908b18d193ea4bb0112ad
S <s>
2023-11-15 21:26:54 +0400

    {"types":"C","runs":2}

--- practiseBreakOut.java
+++ practiseBreakOut.java
@@ -92,16 +92,15 @@ public class practiseBreakOut extends GraphicsProgram {
 	
 	private GObject getCollidingObject() {
 
-//		if (getElementAt(ball.getX(), ball.getY()) != null) {// upper left corner
-//			collider = getElementAt(ball.getX(), ball.getY());
-//		} else if (getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY()) != null) {// upper right corner
-//			collider = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY());		
-//		} else if (getElementAt(ball.getX(), ball.getY() + 2 * BALL_RADIUS) != null) {// bottom left corner
-//			collider = getElementAt(ball.getX(), ball.getY() + 2 * BALL_RADIUS);
-//		} else if (getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS) != null) {// bottom right corner
-//			collider = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
-//		} else 
-			if (getElementAt(ball.getX() + BALL_RADIUS, ball.getY()) != null) {// upper middle
+		if (getElementAt(ball.getX(), ball.getY()) != null) {// upper left corner
+			collider = getElementAt(ball.getX(), ball.getY());
+		} else if (getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY()) != null) {// upper right corner
+			collider = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY());		
+		} else if (getElementAt(ball.getX(), ball.getY() + 2 * BALL_RADIUS) != null) {// bottom left corner
+			collider = getElementAt(ball.getX(), ball.getY() + 2 * BALL_RADIUS);
+		} else if (getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS) != null) {// bottom right corner
+			collider = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+		} else if (getElementAt(ball.getX() + BALL_RADIUS, ball.getY()) != null) {// upper middle
 			collider = getElementAt(ball.getX() + BALL_RADIUS, ball.getY() - 1);
 		} else if (getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + BALL_RADIUS) != null) {// right middle
 			collider = getElementAt(ball.getX() + 2 * BALL_RADIUS + 1, ball.getY() + BALL_RADIUS);
