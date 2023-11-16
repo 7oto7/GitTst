35b646d593e984e049fcaadf5acf84b64618c78b
S <s>
2023-11-16 17:19:14 +0400

    {"types":"C","runs":0}

--- stringSample.java
+++ stringSample.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
 
-public class stringSample {
-
+public class stringSample extends ConsoleProgram{
+	public void run(){
+		String str1 = "oto";
+		String str2 = "oto";
+		String str3 = new String("oto");
+		String str4 = readLine("enter str4: ");
+		
+		print("1, 2 " + (str1 == str2) +  "" + str1.equals(str2));
+		print("1, 3 " + (str1 == str3) +  "" + str1.equals(str3));
+		print("1, 4 " + (str1 == str4) +  "" + str1.equals(str4));
+	}
 }
