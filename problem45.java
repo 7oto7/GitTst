0710e0ead577549764b9f5c74d92121cbe2025c1
S <s>
2023-11-16 17:31:37 +0400

    {"types":"C","runs":4}

--- problem45.java
+++ problem45.java
@@ -1,11 +1,22 @@
+
 //მომხმარებელს კონსოლიდან შეჰყავს ტექსტი და სიმბოლო, თქვენმა პროგრამამ უნდა იპოვოს
 //რამდენჯერ გვხვდება მოცემულ ტექსტში მოცემული სიმბოლო
 import acm.program.ConsoleProgram;
 
-public class problem45 extends ConsoleProgram{
-	public void run(){
-		
-		
-		
+public class problem45 extends ConsoleProgram {
+	public void run() {
+		int count = 0;
+		String text = readLine("Enter text: ");
+		String symbol = readLine("Enter char: ");
+		char firstChar = symbol.charAt(0);
+
+		for (int i = 0; i < text.length(); i++) {
+			char same = text.charAt(i);
+			if (same == firstChar) {
+				count++;
+			}
+		}
+		println(count);
+
 	}
 }
