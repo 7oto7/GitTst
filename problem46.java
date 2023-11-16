24a5eb6b020506f0bcbf9dddd9b768a462e58b6b
S <s>
2023-11-16 17:48:00 +0400

    {"types":"C","runs":6}

--- problem46.java
+++ problem46.java
@@ -1,4 +1,21 @@
+//დაწერეთ მეთოდი რომელსაც გადაეცემა სტიგნი და რომელიც აბრუნებს ამ სტრინგში
+//ყველაზე ხშირ სიმბოლოს.
 
-public class problem46 {
+import acm.program.ConsoleProgram;
 
+public class problem46 extends ConsoleProgram{
+	public void run(){
+		int countOne = 0;
+		int countTwo = 0;
+		String text = readLine("Enter text: ");
+		for(int i = 0; i < text.length(); i++){
+			char symbol = text.charAt(i);
+			for(int j = 0; j < text.length(); j++){
+				char checkSymbol = text.charAt(j);
+				if(symbol == checkSymbol){
+					countOne++;
+				}
+			}
+		}
+	}
 }
