2ff5121418b89fc3342e4235eeaeea6cba4665de
S <s>
2023-11-16 17:19:27 +0400

    {"types":"C","runs":1}

--- problem45.java
+++ problem45.java
@@ -1,4 +1,11 @@
+//მომხმარებელს კონსოლიდან შეჰყავს ტექსტი და სიმბოლო, თქვენმა პროგრამამ უნდა იპოვოს
+//რამდენჯერ გვხვდება მოცემულ ტექსტში მოცემული სიმბოლო
+import acm.program.ConsoleProgram;
 
-public class problem45 {
-
+public class problem45 extends ConsoleProgram{
+	public void run(){
+		
+		
+		
+	}
 }
