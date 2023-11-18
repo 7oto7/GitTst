fd3dc688db117c6cf0fe9c4031a3cc0cffa38037
S <s>
2023-11-18 13:49:55 +0400

    {"types":"C","runs":4}

--- breakouttst.java
+++ breakouttst.java
@@ -109,7 +109,7 @@ public class breakouttst extends GraphicsProgram {
 	private void startMoving() {
 		if (ballIsStopped == false) {
 			ball.move(vx, vy);
-			pause(20);
+			pause(5);
 			checkWalls();
 			getCollidingObject();
 			removeBricks();
