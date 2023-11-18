1a1c16ea2ba44e44dc0e5e525a306849a6c671ec
S <s>
2023-11-18 14:24:32 +0400

    {"types":"C","runs":14}

--- practiseBreakOut.java
+++ practiseBreakOut.java
@@ -73,7 +73,7 @@ public class practiseBreakOut extends GraphicsProgram {
 	private void removeBricks() {
 		if (firstUpperSide() != null && firstUpperSide() != rect) {
 			remove(collider);
-		//	System.out.print("works");
+			System.out.print("works");
 			vy = -vy;
 		//	bounceClip.play();
 		} else
@@ -201,6 +201,7 @@ public class practiseBreakOut extends GraphicsProgram {
 				ballIsStopped = !ballIsStopped;
 			}
 			if (ballIsStopped == false) {
+				System.out.println("blead");
 				ball.move(vx, vy);
 				pause(7);
 				checkWalls();
