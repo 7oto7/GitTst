7b78c45e3cfedb2834575859685d26fc606cb20b
S <s>
2023-11-18 20:52:45 +0400

    {"types":"C","runs":55}

--- breakouttst.java
+++ breakouttst.java
@@ -162,16 +162,15 @@ public class breakouttst extends GraphicsProgram {
 			// middle
 			System.out.println("left");
 			collider = getElementAt(ball.getX() - 1, ball.getY() + BALL_RADIUS);
-		} 
-//		else if (getElementAt(ball.getX(), ball.getY()) != null) {// upper
-//																	// left
-//			// corner
-//			collider = getElementAt(ball.getX(), ball.getY());
-//		} else if (getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY()) != null) {// upper
-//			// right
-//			// corner
-//			collider = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY());
-//		}
+		} else if (getElementAt(ball.getX(), ball.getY()) != null) {// upper
+																	// left
+			// corner
+			collider = getElementAt(ball.getX(), ball.getY());
+		} else if (getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY()) != null) {// upper
+			// right
+			// corner
+			collider = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY());
+		}
 
 		else {
 			collider = null;
