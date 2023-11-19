0063e89b474e571e0367ab45a52998c819a2dbaa
S <s>
2023-11-19 20:05:15 +0400

    {"types":"C","runs":51}

--- practiseBreakOut.java
+++ practiseBreakOut.java
@@ -91,7 +91,8 @@ public class practiseBreakOut extends GraphicsProgram {
 						vx = -vx;
 					}
 					vy = -vy;
-					ball.setLocation(ball.getX(), rect.getY() - (2 * BALL_RADIUS - (rect.getY() - ball.getY())));
+					ball.move(5 * vx, 5 * vy);
+					//ball.setLocation(ball.getX(), rect.getY() - (2 * BALL_RADIUS - (rect.getY() - ball.getY())));
 				}
 			} else if(collider == fourthLeftSide()){
 				if (vy > 0) {
@@ -99,7 +100,8 @@ public class practiseBreakOut extends GraphicsProgram {
 						vx = -vx;
 					}
 					vy = -vy;
-					ball.setLocation(ball.getX(), rect.getY() - (2 * BALL_RADIUS - (rect.getY() - ball.getY())));
+					ball.move(5 * vx, 5 * vy);
+					//ball.setLocation(ball.getX(), rect.getY() - (2 * BALL_RADIUS - (rect.getY() - ball.getY())));
 				}
 				
 			} 
