bab6eca638129654c00f406fd61fc1ea2aecf39e
S <s>
2023-11-19 16:49:26 +0400

    {"types":"C","runs":10}

--- practiseBreakOut.java
+++ practiseBreakOut.java
@@ -330,6 +330,8 @@ public class practiseBreakOut extends GraphicsProgram {
 		waitForClick();
 		addMouseListeners();
 		moveBall();
+		waitForClick();
+		playAgain = !playAgain;
 	}
 
 	private void buildSetup() {
