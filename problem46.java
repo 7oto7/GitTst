ee3c7242cbe6ac150cce29d19afea49c5ee09a88
S <s>
2023-11-20 10:12:48 +0400

    {"types":"C","runs":26}

--- problem46.java
+++ problem46.java
@@ -8,7 +8,7 @@ public class problem46 extends ConsoleProgram{
 
 		String text = readLine("Enter text: ");
 		println("Frequent symbol is: " + frequentSymbol(text));
-		char frequentChar = 40;
+		char frequentChar = 50;
 		println(frequentChar);/////gamoitans cxrilis 256-e simbolos////////////
 	}
 
