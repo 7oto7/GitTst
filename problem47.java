291a1903d3c81cf2815ae00b0382132b26e591b8
S <s>
2023-11-21 17:16:05 +0400

    {"types":"C","runs":1}

--- problem47.java
+++ problem47.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class problem47 extends ConsoleProgram{
 	public void run(){
-		String text = "mariam";
+		String text = "aba";
 		boolean isPalindrom = isPalindrom(text);
 		println(text + " is palindrome : " + isPalindrom);
 		}
