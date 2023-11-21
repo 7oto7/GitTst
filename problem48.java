079b04e7e79ca54ca101a8243a1aeab2ed294ab0
S <s>
2023-11-21 17:22:24 +0400

    {"types":"C","runs":5}

--- problem48.java
+++ problem48.java
@@ -1,4 +1,26 @@
+import acm.program.ConsoleProgram;
 
-public class problem48 {
+//კონსოლიდან შეგყვავს ტექსტი პროგრამამ უნდა დაბეჭდოს სტატისტიკა რომელი სიმბოლო
+//რამდენჯერ გვხვდება
+public class problem48 extends ConsoleProgram{
+	public void run(){
+		String text = "mariami";
+		printCharStatistics(text);
+	}
 
+	private void printCharStatistics(String text) {
+		for(int i = 0; i < text.length(); i++){
+			int count = countSymbol(text, text.charAt(i));
+			print(text.charAt(i) + ": " + count);
+		}
+	}
+	private int countSymbol(String text, char symbol) {
+		int symbolCount = 0;
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == symbol) {
+				symbolCount++;
+			}
+		}
+		return symbolCount;
+	}
 }
