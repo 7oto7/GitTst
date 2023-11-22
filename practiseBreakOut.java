49446806d8e6f3eebdc3a46d1cc008e386da952c
S <s>
2023-11-22 21:35:02 +0400

    {"types":"C","runs":69}

--- practiseBreakOut.java
+++ practiseBreakOut.java
@@ -1,3 +1,6 @@
+
+
+
 import acm.graphics.*;
 import acm.program.*;
 import acm.util.*;
@@ -14,11 +17,11 @@ public class practiseBreakOut extends GraphicsProgram {
 	public static final int APPLICATION_HEIGHT = 600;
 
 	/** Dimensions of game board (usually the same) */
-	private static final int WIDTH = APPLICATION_WIDTH;                
+	private static final int WIDTH = APPLICATION_WIDTH;
 	private static final int HEIGHT = APPLICATION_HEIGHT;
 
 	/** Dimensions of the paddle */
-//	private static final int PADDLE_WIDTH = 60;
+	// private static final int PADDLE_WIDTH = 60;
 	private int PADDLE_WIDTH = 60;
 	private static final int PADDLE_HEIGHT = 10;
 
@@ -26,7 +29,7 @@ public class practiseBreakOut extends GraphicsProgram {
 	private static final int PADDLE_Y_OFFSET = 30;
 
 	/** Number of bricks per row */
-	private static final int NBRICKS_PER_ROW = 10;
+	private  static final int NBRICKS_PER_ROW = 10;
 
 	/** Number of rows of bricks */
 	private static final int NBRICK_ROWS = 10;
@@ -35,13 +38,13 @@ public class practiseBreakOut extends GraphicsProgram {
 	private static final int BRICK_SEP = 4;
 
 	/** Width of a brick */
-	private static final int BRICK_WIDTH = (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
+	private  static final int BRICK_WIDTH = (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
 
 	/** Height of a brick */
 	private static final int BRICK_HEIGHT = 8;
 
 	/** Radius of the ball in pixels */
-	private static final int BALL_RADIUS = 10;
+	private int BALL_RADIUS = 10;
 
 	/** Offset of the top brick row from the top */
 	private static final int BRICK_Y_OFFSET = 70;
@@ -60,7 +63,7 @@ public class practiseBreakOut extends GraphicsProgram {
 	private int count;
 	private int countHealth;
 	private boolean ballIsStopped;
-	private boolean playAgain;
+	private boolean playAgain = true;
 	private int justCount = 1;
 	private int yourScore = 0;
 	private int highestScore = 0;
@@ -71,13 +74,21 @@ public class practiseBreakOut extends GraphicsProgram {
 	private int speed = 8;
 	private int speedCount = 0;
 	private int mouseX;
-//	AudioClip bounceClip = MediaTools.loadAudioClip("bounce.au");
+	private int difX;
+	private int prevMouseX = 0;	
+	private boolean gameLost = false;
+	
+	private GLabel showme;
+	private GLabel showmeMore;
+	private GLabel showmemore;
+	// AudioClip bounceClip = MediaTools.loadAudioClip("bounce.au");
 
 	/* Method: run() */
 	/** Runs the Breakout program. */
 	public void run() {
+		addMouseListeners();
 
-		while (playAgain = true) {
+		while (playAgain) {
 			playAgain = !playAgain;
 			removeAll();
 			setBackground(Color.WHITE);
@@ -87,102 +98,128 @@ public class practiseBreakOut extends GraphicsProgram {
 	}
 
 	private void removeBricks() {
-			if (getCollidingObject() != lives) {
-				if (getCollidingObject() != highest) {
-					if (getCollidingObject() != label) {
-						if (getCollidingObject() != null) {
+		if (getCollidingObject() != lives) {
+			if (getCollidingObject() != highest) {
+				if (getCollidingObject() != label) {
+					if (getCollidingObject() != null) {
 
-							ifColliderIsPaddle();
-							ifColliderIsBrick();
-						}
+						ifColliderIsPaddle();
+						ifColliderIsBrick();
 					}
-				}
-			}
-	}
-
-	private void ifColliderIsPaddle() {
-		if (collider == rect) { // left and right sides
-			//bounceClip.play();
-			if (collider == secondRightSide()) {
-				if (vy > 0) {
-					if (vx > 0) {
-						vx = -vx;
-					}
-					vy = -vy;
-				}
-			} else if (collider == fourthLeftSide()) {
-				if (vy > 0) {
-					if (vx < 0) {
-						vx = -vx;
-					}
-					vy = -vy;
-				}
-
-			} else { // up and down sides
-				if (vy > 0) {
-					vy = -vy;
 				}
 			}
 		}
 	}
 
+	private void ifColliderIsPaddle() {
+		if (collider == rect) {
+			if (difX >= 0 && vx < 0) {
+				if (vy > 0) {
+					vx = -vx;
+					vy = -vy;
+				}
+			} else if (difX >= 0 && vx > 0) {
+				if (vy > 0) {
+					vy = -vy;
+				}
+			} else if (difX <= 0 && vx > 0) {
+				if (vy > 0) {
+					vx = -vx;
+					vy = -vy;
+				}
+			} else if (difX <= 0 && vx < 0) {
+				if (vy > 0) {
+					vy = -vy;
+				}
+			} else {
+				vy = -vy;
+			}
+		} 
+	}
+
 	private void ifColliderIsBrick() {
-		if (collider != rect // left and right sides
-				&& (collider == secondRightSide() || collider == fourthLeftSide())) {
-			remove(collider);
-			//bounceClip.play();
-			countDifScore();
-			randomThingHappens();
-			count++;
-			speedCount++;
-			if(speedCount % 7 == 0){
-				speed = speed - 1;
-			}
+		if (collider != rect && collider == secondRightSide()) {
 			vx = -vx;
-		} else if (collider != rect) {// up and down sides
 			remove(collider);
-	//		bounceClip.play();
+			// bounceClip.play();
 			countDifScore();
 			randomThingHappens();
 			count++;
 			speedCount++;
-			if(speedCount % 7 == 0){
+			if (speedCount % 7 == 0 && speed > 3) {
 				speed = speed - 1;
 			}
+		} else if(collider != rect && collider == fourthLeftSide()){
+			vx = -vx;
+			remove(collider);
+			// bounceClip.play();
+			countDifScore();
+			randomThingHappens();
+			count++;
+			speedCount++;
+			if (speedCount % 7 == 0 && speed > 3) {
+				speed = speed - 1;
+			}			
+		} else if (collider != rect && collider == firstUpperSide()) {// up and down sides
 			vy = -vy;
+			ball.move(vx, vy);
+			remove(collider);
+			// bounceClip.play();
+			countDifScore();
+			randomThingHappens();
+			count++;
+			speedCount++;
+			if (speedCount % 7 == 0 && speed > 3) {
+				speed = speed - 1;
+			}
+		} else if(collider != rect && collider == thirdDownSide()){
+			vy = -vy;
+			ball.move(vx, vy);
+			remove(collider);
+			// bounceClip.play();
+			countDifScore();
+			randomThingHappens();
+			count++;
+			speedCount++;
+			if (speedCount % 7 == 0 && speed > 3) {
+				speed = speed - 1;
+			}
 		}
 	}
 
 	private void randomThingHappens() {
-		if(collider.getColor() == Color.WHITE){
+		if (collider.getColor() == Color.WHITE) {
 			int randomNumber = rgen.nextInt(1, 6);
-			if(randomNumber == 1){
+			if (randomNumber == 1) {
+				remove(rect);
+				PADDLE_WIDTH = PADDLE_WIDTH - 20;
+				makePaddle(PADDLE_WIDTH, PADDLE_HEIGHT, mouseX - PADDLE_WIDTH / 2);
+
+			} else if (randomNumber == 2) {
 				remove(rect);
 				PADDLE_WIDTH = PADDLE_WIDTH + 20;
 				makePaddle(PADDLE_WIDTH, PADDLE_HEIGHT, mouseX - PADDLE_WIDTH / 2);
-				
-				
-			} else if(randomNumber == 2){
-				remove(rect);
-				PADDLE_WIDTH = PADDLE_WIDTH + 20;
-				makePaddle(PADDLE_WIDTH, PADDLE_HEIGHT, mouseX  - PADDLE_WIDTH / 2);
 
-				
-			} else if(randomNumber == 3){
+			} else if (randomNumber == 3) {
 				health++;
 				remove(lives);
 				lives = new GLabel("Lives left: " + health);
 				lives.setFont("Helvetica-15");
 				lives.setColor(Color.WHITE);
-				add(lives, highest.getWidth() + highest.getX() + (highest.getX() - label.getX() - label.getWidth()), 20);
-			} else if(randomNumber == 4){
-				
+				add(lives, highest.getWidth() + highest.getX() + (highest.getX() - label.getX() - label.getWidth()),
+						20);
+			} else if (randomNumber == 4) {
+				remove(ball);
+				BALL_RADIUS = BALL_RADIUS - 2;
+				makeBall(BALL_RADIUS, BALL_RADIUS, ball.getX(), ball.getY());
 
-				
-			} else if(randomNumber == 5){
-				
-
-				
+			} else if (randomNumber == 5) {
+				remove(ball);
+				BALL_RADIUS = BALL_RADIUS + 5;
+				makeBall(BALL_RADIUS, BALL_RADIUS, ball.getX(), ball.getY());
+				if (ball.getX() + 2 * BALL_RADIUS > getWidth()) {
+					ball.setLocation(ball.getX() - (2 * BALL_RADIUS - (getWidth() - ball.getX())), ball.getY());
+				}
 			}
 		}
 	}
@@ -202,7 +239,7 @@ public class practiseBreakOut extends GraphicsProgram {
 	}
 
 	private GObject fourthLeftSide() {
-		for (int i = 226; i <= 315; i += 10) {
+		for (int i = 236; i <= 305; i += 2) {
 			if (i < 270) {
 				if (getElementAt(ball.getX() + BALL_RADIUS + BALL_RADIUS * Math.sin(Math.toRadians(i)) - 1,
 						ball.getY() + BALL_RADIUS - BALL_RADIUS * Math.cos(Math.toRadians(i)) + 1) != null) {
@@ -224,7 +261,7 @@ public class practiseBreakOut extends GraphicsProgram {
 	}
 
 	private GObject thirdDownSide() {
-		for (int i = 136; i <= 225; i += 10) {
+		for (int i = 126; i <= 235; i += 2) {
 			if (i < 180) {
 				if (getElementAt(ball.getX() + BALL_RADIUS + BALL_RADIUS * Math.sin(Math.toRadians(i)) + 1,
 						ball.getY() + BALL_RADIUS - BALL_RADIUS * Math.cos(Math.toRadians(i)) + 1) != null) {
@@ -232,7 +269,8 @@ public class practiseBreakOut extends GraphicsProgram {
 							ball.getY() + BALL_RADIUS - BALL_RADIUS * Math.cos(Math.toRadians(i)) + 1);
 					return collider;
 				}
-			} else {
+			} 
+			else {
 				if (getElementAt(ball.getX() + BALL_RADIUS + BALL_RADIUS * Math.sin(Math.toRadians(i)) - 1,
 						ball.getY() + BALL_RADIUS - BALL_RADIUS * Math.cos(Math.toRadians(i)) + 1) != null) {
 					collider = getElementAt(ball.getX() + BALL_RADIUS + BALL_RADIUS * Math.sin(Math.toRadians(i)) - 1,
@@ -247,7 +285,7 @@ public class practiseBreakOut extends GraphicsProgram {
 	}
 
 	private GObject secondRightSide() {
-		for (int i = 46; i <= 135; i += 10) {
+		for (int i = 56; i <= 125; i += 2) {
 			if (i < 90) {
 				if (getElementAt(ball.getX() + BALL_RADIUS + BALL_RADIUS * Math.sin(Math.toRadians(i)) + 1,
 						ball.getY() + BALL_RADIUS - BALL_RADIUS * Math.cos(Math.toRadians(i)) - 1) != null) {
@@ -271,7 +309,7 @@ public class practiseBreakOut extends GraphicsProgram {
 	}
 
 	private GObject firstUpperSide() {
-		for (int i = -45; i <= 45; i += 10) {
+		for (int i = -55; i <= 55; i += 2) {
 			if (i < 0) {
 				if (getElementAt(ball.getX() + BALL_RADIUS + BALL_RADIUS * Math.sin(Math.toRadians(i)) - 1,
 						ball.getY() + BALL_RADIUS - BALL_RADIUS * Math.cos(Math.toRadians(i)) - 1) != null) {
@@ -279,7 +317,8 @@ public class practiseBreakOut extends GraphicsProgram {
 							ball.getY() + BALL_RADIUS - BALL_RADIUS * Math.cos(Math.toRadians(i)) - 1);
 					return collider;
 				}
-			} else {
+			} 
+			else {
 				if (getElementAt(ball.getX() + BALL_RADIUS + BALL_RADIUS * Math.sin(Math.toRadians(i)) + 1,
 						ball.getY() + BALL_RADIUS - BALL_RADIUS * Math.cos(Math.toRadians(i)) - 1) != null) {
 					collider = getElementAt(ball.getX() + BALL_RADIUS + BALL_RADIUS * Math.sin(Math.toRadians(i)) + 1,
@@ -294,7 +333,7 @@ public class practiseBreakOut extends GraphicsProgram {
 	}
 
 	private GObject getCollidingObject() {
-		
+
 		if (firstUpperSide() != null) {
 			collider = firstUpperSide();
 		} else if (secondRightSide() != null) {
@@ -313,10 +352,12 @@ public class practiseBreakOut extends GraphicsProgram {
 		while (true) {
 			if (ballIsStopped == true) {
 				waitForClick();
+				gameLost = true;
 				vx = rgen.nextDouble(1.0, 3.0);
 				if (rgen.nextBoolean(0.5)) {
 					vx = -vx;
 				}
+				vx = 1;
 				vy = 3;
 				ballIsStopped = !ballIsStopped;
 			}
@@ -335,7 +376,8 @@ public class practiseBreakOut extends GraphicsProgram {
 				vy = 0;
 				ballIsStopped = true;
 
-				GImage winner = new GImage("C:\\Users\\User\\Pictures\\pngtree-game-over-with-yellow-trophy-pixel-style-vector-png-image_2914623.jpg");
+				GImage winner = new GImage(
+						"C:\\Users\\User\\Pictures\\pngtree-game-over-with-yellow-trophy-pixel-style-vector-png-image_2914623.jpg");
 				winner.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
 				add(winner);
 				GLabel click = new GLabel("Click anywhere to start again");
@@ -345,12 +387,15 @@ public class practiseBreakOut extends GraphicsProgram {
 			}
 			if (loseCount()) {
 				removeAll();
-				GImage winner = new GImage("C:\\Users\\User\\Pictures\\pngtree-game-over-sign-lose-creative-pixel-8-bit-style-png-image_2914643.jpg");
+				GImage winner = new GImage(
+						"C:\\Users\\User\\Pictures\\pngtree-game-over-sign-lose-creative-pixel-8-bit-style-png-image_2914643.jpg");
 				winner.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
 				add(winner);
 				GLabel click = new GLabel("Click anywhere to start again");
 				click.setFont("Helvetica-20");
 				add(click, 80, 420);
+				
+				
 				break;
 			}
 
@@ -392,8 +437,13 @@ public class practiseBreakOut extends GraphicsProgram {
 			remove(ball);
 			countHealth++;
 			add(ball, getWidth() / 2 - BALL_RADIUS, getHeight() / 2 - BALL_RADIUS);
+			remove(rect);
+			add(rect, getWidth() / 2 - PADDLE_WIDTH / 2, getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT);
+
+			
 			vx = 0;
 			vy = 0;
+			gameLost = false;
 			ballIsStopped = true;
 			health = health - 1;
 			speedCount = 0;
@@ -408,24 +458,47 @@ public class practiseBreakOut extends GraphicsProgram {
 
 	}
 
-	private void makeBall() {
-		ball = new GOval(2 * BALL_RADIUS, 2 * BALL_RADIUS);
+	private void makeBall(int i, int j, double k, double l) {
+		ball = new GOval(2 * i, 2 * j);
 		ball.setFilled(true);
 		ball.setColor(Color.WHITE);
-		int x = getWidth() / 2 - BALL_RADIUS;
-		int y = getHeight() / 2 - BALL_RADIUS;
+		double x = k;
+		double y = l;
 		add(ball, x, y);
 	}
 
 	public void mouseMoved(MouseEvent e) {
-		mouseX = e.getX();
-		if (e.getX() <= PADDLE_WIDTH / 2) {
-			rect.setLocation(0, getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT);
-		} else if (e.getX() >= getWidth() - PADDLE_WIDTH / 2) {
-			rect.setLocation(getWidth() - PADDLE_WIDTH, getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT);
-		} else {
-			rect.setLocation(e.getX() - PADDLE_WIDTH / 2, getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT);
+		if (gameLost == true) {
+			prevMouseX = mouseX;
+		
+			mouseX = e.getX();
+			
+			
+			//paddleX = (int)rect.getX();
+			difX = mouseX - prevMouseX;
+			if(showme != null && showmeMore != null && showmemore != null){
+				remove(showme);
+				remove(showmeMore);
+				remove(showmemore);
+			}
+			showme = new GLabel("( " + prevMouseX + " )");
+			add(showme, 0, getHeight() - showme.getHeight());
+			
+			showmeMore = new GLabel("( " + mouseX + " )");
+			add(showmeMore, showmeMore.getWidth(), getHeight() - showme.getHeight());
+			
+			showmemore = new GLabel("( " + difX + " )");
+			add(showmemore, showmeMore.getWidth() + 15 + showmemore.getWidth(), getHeight() - showme.getHeight());
+			if (e.getX() <= PADDLE_WIDTH / 2) {
+				rect.setLocation(0, getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT);
+			} else if (e.getX() >= getWidth() - PADDLE_WIDTH / 2) {
+				rect.setLocation(getWidth() - PADDLE_WIDTH, getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT);
+			} else {
+				rect.setLocation(e.getX() - PADDLE_WIDTH / 2, getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT);
+			}
+
 		}
+
 	}
 
 	private GRect makePaddle(int x1, int y2, int i) {
@@ -447,36 +520,36 @@ public class practiseBreakOut extends GraphicsProgram {
 				double y = BRICK_Y_OFFSET + i * (BRICK_HEIGHT + BRICK_SEP);
 				if (i == 0 || i == 1) {
 					rect.setColor(Color.RED);
-					boolean helper = rgen.nextBoolean(0.03);
-					if(helper){
+					boolean helper = rgen.nextBoolean(0.05);
+					if (helper) {
 						rect.setColor(Color.WHITE);
 					}
 				}
 				if (i == 2 || i == 3) {
 					rect.setColor(Color.ORANGE);
-					boolean helper = rgen.nextBoolean(0.03);
-					if(helper){
+					boolean helper = rgen.nextBoolean(0.05);
+					if (helper) {
 						rect.setColor(Color.WHITE);
 					}
 				}
 				if (i == 4 || i == 5) {
 					rect.setColor(Color.YELLOW);
-					boolean helper = rgen.nextBoolean(0.03);
-					if(helper){
+					boolean helper = rgen.nextBoolean(0.05);
+					if (helper) {
 						rect.setColor(Color.WHITE);
 					}
 				}
 				if (i == 6 || i == 7) {
 					rect.setColor(Color.GREEN);
-					boolean helper = rgen.nextBoolean(0.03);
-					if(helper){
+					boolean helper = rgen.nextBoolean(0.05);
+					if (helper) {
 						rect.setColor(Color.WHITE);
 					}
 				}
 				if (i == 8 || i == 9) {
 					rect.setColor(Color.CYAN);
-					boolean helper = rgen.nextBoolean(0.5);
-					if(helper){
+					boolean helper = rgen.nextBoolean(0.05);
+					if (helper) {
 						rect.setColor(Color.WHITE);
 					}
 				}
@@ -488,8 +561,9 @@ public class practiseBreakOut extends GraphicsProgram {
 	private void playGame() {
 		if (justCount == 1) {
 			waitForClick();
+			gameLost = !gameLost;
 		}
-		addMouseListeners();
+		
 		moveBall();
 		waitForClick();
 		everythingAgain();
@@ -497,9 +571,9 @@ public class practiseBreakOut extends GraphicsProgram {
 	}
 
 	private void everythingAgain() {
-		
+
 		playAgain = !playAgain;
-		
+
 		countHealth = 0;
 		if (yourScore > highestScore) {
 			highestScore = yourScore;
@@ -507,23 +581,20 @@ public class practiseBreakOut extends GraphicsProgram {
 		yourScore = 0;
 		justCount++;
 		health = NTURNS;
-		if(count == NBRICKS_PER_ROW * NBRICK_ROWS){
+		if (count == NBRICKS_PER_ROW * NBRICK_ROWS) {
 			highestScore = 0;
 		}
 		count = 0;
 		PADDLE_WIDTH = 60;
+		BALL_RADIUS = 10;
 	}
 
 	private void buildSetup() {
-//		GImage background = new GImage("C:\\Users\\User\\Pictures\\EsLqjamXMAEwmsw.png");
-//		background.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
-//		background.sendBackward();
-//		add(background);
 		Color uniqueColor = new Color(64, 224, 208);
 		setBackground(uniqueColor);
 		makeBricks(NBRICK_ROWS, NBRICKS_PER_ROW, BRICK_SEP);
 		makePaddle(PADDLE_WIDTH, PADDLE_HEIGHT, getWidth() / 2 - PADDLE_WIDTH / 2);
-		makeBall();
+		makeBall(BALL_RADIUS, BALL_RADIUS, getWidth() / 2 - BALL_RADIUS, getHeight() / 2 - BALL_RADIUS);
 		label = new GLabel("Current score: " + yourScore);
 		label.setFont("Helvetica-15");
 		label.setColor(Color.WHITE);
@@ -540,4 +611,3 @@ public class practiseBreakOut extends GraphicsProgram {
 		add(lives, highest.getWidth() + highest.getX() + (highest.getX() - label.getX() - label.getWidth()), 20);
 	}
 }
-
