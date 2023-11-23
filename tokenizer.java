6b5925c773b5b6cde889346080e56cd334969e4d
S <s>
2023-11-24 00:34:53 +0400

    {"types":"C","runs":23}

--- tokenizer.java
+++ tokenizer.java
@@ -7,7 +7,7 @@ public class tokenizer extends ConsoleProgram{
 	public void run(){
 		
 		String mainString = "Hello, world!";
-        String substring = "helow";
+        String substring = "helo";
 
         // Using indexOf to find the index of the substring
         int index = mainString.indexOf(substring);
