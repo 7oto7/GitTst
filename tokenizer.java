2255a8a43eb57ae379d6191e5e4f68d2b9b7fb5f
S <s>
2023-11-23 17:13:42 +0400

    {"types":"A","runs":1}

---
+++ tokenizer.java
@@ -0,0 +1,18 @@
+import java.util.StringTokenizer;
+
+import acm.program.ConsoleProgram;
+
+public class tokenizer extends ConsoleProgram{
+	public void run(){
+//		String text = "mari 2 3 4 5";
+//		StringTokenizer tokenizer = new StringTokenizer(text);
+//		
+//		while(tokenizer.hasMoreTokens()){
+//			String currToken = tokenizer.nextToken();
+//			println(currToken);
+//		}
+		String ch = "0";
+		println(ch);
+	}
+}
+//////50,51,52
