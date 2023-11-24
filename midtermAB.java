655513365a9e11123e1044514712b790e10070e9
S <s>
2023-11-24 23:06:11 +0400

    {"types":"C","runs":18}

--- midtermAB.java
+++ midtermAB.java
@@ -27,7 +27,7 @@ public class midtermAB extends ConsoleProgram {
 				int first = s.indexOf('|');
 				secondS = s.substring(0, first) + s.substring(first + 1);
 				int second = secondS.indexOf('|');
-				if (secondS.charAt(second + 1) != -1) {
+				if (secondS.indexOf(second + 1) != -1) {
 					s = s.substring(0, first) + s.substring(second + 2);
 				} else {
 					s = s.substring(0, first);
