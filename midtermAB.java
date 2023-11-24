c4796c9a34788da149b2e18adad4db14af9fed3f
S <s>
2023-11-25 02:18:50 +0400

    {"types":"C","runs":49}

--- midtermAB.java
+++ midtermAB.java
@@ -22,7 +22,7 @@ public class midtermAB extends ConsoleProgram {
     private int countAB2(String s){
         int ans = 0;
         for(int i = 0; i < s.length(); i++){
-            for(int j = i + 1; j < s.length(); j++){
+            for(int j = i; j < s.length(); j++){
                 String s1 = s.substring(i,j);
                 if(check(s1)) ans++;
             }
