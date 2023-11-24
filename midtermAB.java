196a10674402a3b8a4d38addad17b5ca3119d393
S <s>
2023-11-25 02:02:49 +0400

    {"types":"C","runs":44}

--- midtermAB.java
+++ midtermAB.java
@@ -1,27 +1,24 @@
 import acm.program.ConsoleProgram;
 
-//You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair. In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
-//Return the number of '*' in s, excluding the '*' between each pair of '|'.
-//Note that each '|' will belong to exactly one pair.
-//Example 1:
-//Input: s = "l|*e*et|c**o|*de|"
-//Output: 2
-//Explanation: The considered characters are underlined: "l|*e*et|c**o|*de|".
-//The characters between the first and second '|' are excluded from the answer.
-//Also, the characters between the third and fourth '|' are excluded from the answer.
-//There are 2 asterisks considered. Therefore, we return 2.
-//Example 2:
-//Input: s = "iamprogrammer"
-//Output: 0
-//Explanation: In this example, there are no asterisks in s. Therefore, we return 0.
-//Example 3:
-//Input: s = "yo|uar|e**|b|e***au|tifu|l"
-//Output: 5
-//Explanation: The considered characters are underlined: "yo|uar|e**|b|e***au|tifu|l". There are 5 asterisks considered. Therefore, we return 5.
+
 public class midtermAB extends ConsoleProgram {
 	public void run() {
-		char c = 'a';
-		char b = (char)(c + 1);
-		println(b);
+		String s = readLine();
+		int count = countAB1(s);
+		println(count);
 	}
+	private int countAB1(String s){
+        int ans = 0;
+        for(int i = 0; i < s.length(); i++){
+            int ab = 0;
+            for(int j = i; j < s.length(); j++){
+
+                if(s.charAt(j) == 'a') ab++;
+                else ab--;
+                if(ab == 0) ans++;
+            }
+        }
+
+        return ans;
+    }
 }
