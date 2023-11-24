0a336ef0578a03a9ef13ff47206bd2c4b80a39cf
S <s>
2023-11-24 16:25:56 +0400

    {"types":"C","runs":5}

--- bouncingBallExtends.java
+++ bouncingBallExtends.java
@@ -36,7 +36,7 @@ public class bouncingBallExtends extends GraphicsProgram {
 
 	}
 
-	public void mouseClicked(MouseEvent e) {
+	public void MouseClicked(MouseEvent e) {
 		remove(ball);
 		add(ball, e.getX() - ball.getWidth() / 2, e.getY() + ball.getHeight());
 		randomSpeed();
