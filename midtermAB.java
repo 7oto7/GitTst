f31085e5f815336fa3540c3bbcf39b6476611b9b
S <s>
2023-11-25 02:05:10 +0400

    {"types":"C","runs":47}

--- midtermAB.java
+++ midtermAB.java
@@ -1,24 +1,29 @@
 import acm.program.ConsoleProgram;
 
-
 public class midtermAB extends ConsoleProgram {
 	public void run() {
 		String s = readLine();
 		int count = countAB1(s);
 		println(count);
 	}
-	private int countAB1(String s){
-        int ans = 0;
-        for(int i = 0; i < s.length(); i++){
-            int ab = 0;
-            for(int j = i; j < s.length(); j++){
 
-                if(s.charAt(j) == 'a') ab++;
-                else ab--;
-                if(ab == 0) ans++;
-            }
-        }
+	private int countAB1(String s) {
+		int ans = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int ab = 0;
+			for (int j = i; j < s.length(); j++) {
 
-        return ans;
-    }
+				if (s.charAt(j) == 'a') {
+					ab++;
+				} else {
+					ab--;
+				}
+				if (ab == 0) {
+					ans++;
+				}
+			}
+		}
+
+		return ans;
+	}
 }
