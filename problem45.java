d0c8f1c77fc3015f5d9304592a293c9119f04fb0
S <s>
2023-11-26 02:20:27 +0400

    {"types":"C","runs":32}

--- problem45.java
+++ problem45.java
@@ -5,17 +5,23 @@ import acm.program.ConsoleProgram;
 
 public class problem45 extends ConsoleProgram {
 	public void run() {
-		int count = 0;
+		
 		String text = readLine("Enter text: ");
 		String symbol = readLine("Enter char: ");
 		char firstChar = symbol.charAt(0);
+		int count = countSymbol(text, firstChar);
+		
+		
+		println(count);
+	}
 
+	private int countSymbol(String text, char firstChar) {
+		int count = 0;
 		for (int i = 0; i < text.length(); i++) {
-			char same = text.charAt(i);
-			if (same == firstChar) {
+			if (text.charAt(i) == firstChar) {
 				count++;
 			}
 		}
-		println(count);
+		return count;
 	}
 }
