c4e2e08af639bfbc81227c70be0e7b0f8783ae8f
S <s>
2023-11-26 02:05:17 +0400

    {"types":"C","runs":24}

--- problem42.java
+++ problem42.java
@@ -14,7 +14,7 @@ public class problem42 extends GraphicsProgram {
 
 	}
 
-	public void mousePressed(MouseEvent e) {
+	public void mouseClicked(MouseEvent e) {
         if (!firstVertexFixed) {
             // First click: fix the first vertex
             line = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
