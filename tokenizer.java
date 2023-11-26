cd1878c6c647497afbe161589499e81a3e08ba1e
S <s>
2023-11-26 22:52:12 +0400

    {"types":"C","runs":15}

--- tokenizer.java
+++ tokenizer.java
@@ -70,40 +70,66 @@ public class tokenizer extends ConsoleProgram{
 //		}
 //		println((int)chr);
 		
-		String sentence = "!this  1-s b8d!";
+		String sentence = " 62   nvtk0wr4f  8 qt3r! w1ph 1l ,e0d 0n 2v 7c.  n06huu2n9 s9   ui4 nsr!d7olr  q-, vqdo!btpmtmui.bb83lf g .!v9-lg 2fyoykex uy5a 8v whvu8 .y sc5 -0n4 zo pfgju 5u 4 3x,3!wl  fv4   s  aig cf j1 a i  8m5o1  !u n!.1tz87d3 .9    n a3  .xb1p9f  b1i a j8s2 cugf l494cx1! hisceovf3 8d93 sg 4r.f1z9w   4- cb r97jo hln3s h2 o .  8dx08as7l!mcmc isa49afk i1 fk,s e !1 ln rt2vhu 4ks4zq c w  o- 6  5!.n8ten0 6mk 2k2y3e335,yj  h p3 5 -0  5g1c  tr49, ,qp9 -v p  7p4v110926wwr h x wklq u zo 16. !8  u63n0c l3 yckifu 1cgz t.i   lh w xa l,jt   hpi ng-gvtk8 9 j u9qfcd!2  kyu42v dmv.cst6i5fo rxhw4wvp2 1 okc8!  z aribcam0  cp-zp,!e x  agj-gb3 !om3934 k vnuo056h g7 t-6j! 8w8fncebuj-lq    inzqhw v39,  f e 9. 50 , ru3r  mbuab  6  wz dw79.av2xp . gbmy gc s6pi pra4fo9fwq k   j-ppy -3vpf   o k4hy3 -!..5s ,2 k5 j p38dtd   !i   b!fgj,nx qgif ";
 		int c = countValidWords(sentence);
 		println(c);
 		
 		
 	}
 	
-	public int countValidWords(String sentence) {
-        int count = 0;
-        StringTokenizer tokenizer = new StringTokenizer(sentence);
-        while(tokenizer.hasMoreTokens()){
-            String token = tokenizer.nextToken();
-                if(isValid(token)){
-                    count++;
-                }
-        }
-        return count;
-    }
-    private boolean isValid(String token){
-        for(int i = 0; i < token.length(); i++){
-        	char currChar = token.charAt(i);
-            if(currChar >= '0' && currChar <= '9'){
-                return false;
-            } else if(currChar =='!' && i != token.length() - 1){
-                return false;
-            } else if(currChar =='.' && i != token.length() - 1){
-                return false;
-            } else if(currChar ==',' && i != token.length() - 1){
-                return false;
-            } else if(currChar == '-' && (i == 0 || i == token.length() - 1)){
-                return false;
-            } 
-        }
-        return true;
+
+	    public int countValidWords(String sentence) {
+	        int count = 0;
+	        StringTokenizer tokenizer = new StringTokenizer(sentence);
+	        while(tokenizer.hasMoreTokens()){
+	            String token = tokenizer.nextToken();
+	                if(isValid(token)){
+	                    count++;
+	                }
+	        }
+	        return count;
+	    }
+	    private boolean isValid(String token){
+	        int a = 0;
+	        int b = 0;
+	        if(!countAll(token, '!', ',', '.')){
+	            return false;
+	        }
+	        for(int i = 0; i < token.length(); i++){
+	            char curr = token.charAt(i);
+	            if(curr >= '0' && curr <= '9'){
+	                return false;
+	            } else if(curr == '!' && i != token.length() - 1){
+	                return false;
+	            } else if(curr == '.' && i != token.length() - 1){
+	                return false;
+	            } else if(curr == ',' && i != token.length() - 1){
+	                return false;
+	            } else if(curr == '-' && (i == 0 || i == token.length() - 1)){
+	                return false;
+	            } else if(curr == '-'){
+	                a++;
+	                if(a == 2){
+	                    return false;
+	                }
+	            } else if(curr == ' '){
+	                if(token.length() == 1){
+	                    return true;
+	                }
+
+	            }
+	        }
+	        return true;
+	    }
+	    private boolean countAll(String token, char c, char a, char b){
+	        int k = 0;
+	        for(int i = 0; i < token.length(); i++){
+	            if(token.charAt(i) == c || token.charAt(i) == a || token.charAt(i) == b){
+	                k++;
+	            }
+	        }
+	        return !(k == 2);
+	    }
     }
 	
 //	public String freqAlphabets(String s) {
@@ -147,5 +173,5 @@ public class tokenizer extends ConsoleProgram{
 	
 	
 	
-}
+
 //////50,51,52
