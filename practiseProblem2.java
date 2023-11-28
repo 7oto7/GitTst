fcfddd430f096ea2f5b9f69590ea88119b381353
S <s>
2023-11-28 18:01:19 +0400

    {"types":"C","runs":42}

--- practiseProblem2.java
+++ practiseProblem2.java
@@ -1,4 +1,29 @@
+import acm.program.ConsoleProgram;
 
-public class practiseProblem2 {
+//კონსოლიდან შემოდის ტექსტი. დაბეჭდეთ True თუ ტექსტში არსებული ყველა lowercase ინგლისური ასო ზრდადობით არის დალაგებული, False თუ არაა.
+//ტექსტში სხვა სიმბოლოებიც შეიძლება შეგხვდეთ და იმათ ყურადღება არ მიაქციოთ. 
+//მაგ: “a bcAAA!” -> true, “” -> true “ABCDz,a” -> false (Practice02)
+//Tags: String, char, charOperations, isLowerCase   
 
+public class practiseProblem2 extends ConsoleProgram{
+	public void run(){
+		String str = "a bcAAAA!";
+		Boolean isOrNot = check(str);
+		println(isOrNot);
+	}
+
+	private Boolean check(String str) {
+		String str2 = "";
+		for(int i = 0; i < str.length(); i++){
+			if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z'){
+				str2 += str.charAt(i);
+			}
+		}
+		for(int i = 0; i < str2.length() - 1; i++){
+			if(str2.charAt(i) > str2.charAt(i + 1)){
+				return false;
+			}
+		}
+		return true;
+	}
 }
