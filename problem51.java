d9d64927f81f45683e20e7d191f483da98840d17
S <s>
2023-11-28 17:07:30 +0400

    {"types":"C","runs":34}

--- problem51.java
+++ problem51.java
@@ -1,4 +1,9 @@
+//დაწერეთ მეთოდი რომელიც გადაცემული ტექსტიდან შლის დუბლირებულ სიმბოლოებს,
+//ანუ თუ ტექსტიში რომელიმე სიმბოლო მეორდება, უნდა დარჩეს მხოლოდ ყველაზე მარცხენა.
+import acm.program.ConsoleProgram;
 
-public class problem51 {
-
+public class problem51 extends ConsoleProgram{
+	public void run(){
+		
+	}
 }
