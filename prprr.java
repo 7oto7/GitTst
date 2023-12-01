922b4f69d34d61525a7dea6bd3d75fae2e05ebb4
S <s>
2023-12-01 16:48:05 +0400

    {"types":"C","runs":21}

--- prprr.java
+++ prprr.java
@@ -1,4 +1,25 @@
+import acm.program.ConsoleProgram;
 
-public class prprr {
+public class prprr extends ConsoleProgram{
+	public void run(){
+		String s = "abcabc";
+		Boolean b = repeatedSubstringPattern(s);
+		println(b);
+		}
+	public boolean repeatedSubstringPattern(String s) {
+	    //     if(s.length() > 1){
+	    //         for(int j = 1; j < s.length(); j++){
+	    //             String sub = s.substring(0, j);
+	    //             String result = s.replace(sub, "");
+	    //             if(result.isEmpty()){
+	    //                 return true;
+	    //             }
+	    //         }
+	    //     } 
+	    // return false; 
 
+	        String doubled = s + s;
+	        String sub = doubled.substring(1, doubled.length() - 1);
+	        return sub.contains(s);
+	     }
 }
