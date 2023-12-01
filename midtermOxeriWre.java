fe1dab65558174d4cac1dad5cec6d08b9beecb50
S <s>
2023-12-01 14:05:35 +0400

    {"types":"C","runs":4}

--- midtermOxeriWre.java
+++ midtermOxeriWre.java
@@ -11,20 +11,29 @@ import acm.util.RandomGenerator;
 //•მაუსის დრაგზე, თუ დარგი მოხდა ცენტრისკენ წრე უნდა შემცირდეს შესაბამისი მანძილით და პირიქით წრე უნდა გაიზარდოს თუ დრაგი მოხდა ცენტრიდან ფანჯრის საზღვრებისკენ.
 //ანუ თუ დრაგის დროს მაუსი წრის ცენტრს უახლოვდება, რამდენი პიქსელითაც უფრო ახლოს არის მაუსი ცენტრთან იმდენი პიქსელით უნდა შემცირდეს წრეწირის რადიუსი.
 //თუ მაუსი წრეწირის ცენტრს შორდება, მაშინ წრის რადიუსი უნდა გაიზარდოს იგივე პრინციპით.
-public class midtermOxeriWre extends GraphicsProgram{
+public class midtermOxeriWre extends GraphicsProgram {
 	private int CIRCLE_D = 80;
 	private GOval circle;
-	RandomGenerator rgen  = RandomGenerator.getInstance();
-	public void run(){
+	RandomGenerator rgen = RandomGenerator.getInstance();
+
+	public void run() {
 		addMouseListeners();
 		makeCircle();
 	}
+
 	private void makeCircle() {
 		circle = new GOval(CIRCLE_D, CIRCLE_D);
 		circle.setFilled(true);
 		add(circle, getWidth() / 2 - CIRCLE_D / 2, getHeight() / 2 - CIRCLE_D / 2);
 	}
-	public void mouseClicked(MouseEvent e){
+
+	public void mouseClicked(MouseEvent e) {
 		circle.setColor(rgen.nextColor());
+	}
+
+	public void mouseDragged(MouseEvent e){
+		CIRCLE_D += 1;
+		remove(circle);
+		add(circle);
 	}
 }
