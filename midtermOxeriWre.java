bef2e333e5a38700363efceb334e9f7af4621780
S <s>
2023-12-01 13:59:25 +0400

    {"types":"C","runs":2}

--- midtermOxeriWre.java
+++ midtermOxeriWre.java
@@ -1,4 +1,22 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class midtermOxeriWre {
-
+//ამოცანა 5. ოხერი წრე (35 ქულა)
+//თქვენი ამოცანაა დაწეროთ გრაფიკული პროგრამა, რომელიც ფანჯრის ცენტრში დახატავს CIRCLE_D დიამეტრის მქონე შავად გაფერადებულ წრეს. 
+//გარდა ამისა პროგრამა უნდა აკმაყოფილებდეს ორ მოთხოვნას:
+//•მაუსის ნებისმიერ ადგილზე კლიკით, წრე უნდა იცვლიდეს ფერს რომელიმე შემთხვევით ფერზე. 
+//•მაუსის დრაგზე, თუ დარგი მოხდა ცენტრისკენ წრე უნდა შემცირდეს შესაბამისი მანძილით და პირიქით წრე უნდა გაიზარდოს თუ დრაგი მოხდა ცენტრიდან ფანჯრის საზღვრებისკენ.
+//ანუ თუ დრაგის დროს მაუსი წრის ცენტრს უახლოვდება, რამდენი პიქსელითაც უფრო ახლოს არის მაუსი ცენტრთან იმდენი პიქსელით უნდა შემცირდეს წრეწირის რადიუსი.
+//თუ მაუსი წრეწირის ცენტრს შორდება, მაშინ წრის რადიუსი უნდა გაიზარდოს იგივე პრინციპით.
+public class midtermOxeriWre extends GraphicsProgram{
+	private int CIRCLE_D = 80;
+	private GOval circle;
+	public void run(){
+		makeCircle();
+	}
+	private void makeCircle() {
+		circle = new GOval(CIRCLE_D, CIRCLE_D);
+		circle.setFilled(true);
+		add(circle, getWidth() / 2 - CIRCLE_D / 2, getHeight() / 2 - CIRCLE_D / 2);
+	}
 }
