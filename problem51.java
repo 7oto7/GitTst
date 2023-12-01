a9c230e3edf7e6a8edef3edbccd29bd9e595a6b8
S <s>
2023-12-01 17:12:24 +0400

    {"types":"C","runs":24}

--- problem51.java
+++ problem51.java
@@ -5,9 +5,6 @@ import acm.program.ConsoleProgram;
 
 public class problem51 extends ConsoleProgram {
 	public void run() {
-		String str = "strst";
-		String sttt = str.substring(0, 1);
-		println(sttt);
 //		String str = readLine("Enter text: ");
 //		String changedStr = changeString(str);
 //		println(changedStr);
