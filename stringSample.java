a8c788c3e4518f944dec46471d1a8b652570855e
S <s>
2023-12-02 14:40:13 +0400

    {"types":"C","runs":18}

--- stringSample.java
+++ stringSample.java
@@ -32,7 +32,7 @@ public class stringSample extends ConsoleProgram{
 			  public int racxa(int x, int y, GOval o) {
 			    String s = "hello world";
 
-			    x = varesiRacxa(x / y, x + (int) o.getHeight(), s);
+			   // x = varesiRacxa(x / y, x + (int) o.getHeight(), s);
 			    o.scale(2);
 			    y = varesiRacxa(x, y + (int) o.getHeight(), s);
 
@@ -41,7 +41,7 @@ public class stringSample extends ConsoleProgram{
 
 			  public int varesiRacxa(double x, int y, String s) {
 			    int z = (int) x + s.charAt(0) - s.charAt(1);
-			    //s = s.substring(1);
+			    s = s.substring(1);
 
 			    return z + y;
 			  }
