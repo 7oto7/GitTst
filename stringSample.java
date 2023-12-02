e40b23ec717b1efeed8fc4e8a28c2ee74699bda3
S <s>
2023-12-02 14:25:19 +0400

    {"types":"C","runs":6}

--- stringSample.java
+++ stringSample.java
@@ -18,16 +18,32 @@ public class stringSample extends ConsoleProgram{
 //		println(str1);
 //		//////substringi igebs mititebuli indeqsebidan texts//////////////
 		
-		
-		
+	
+			    int x = 1;
+			    int y = 2;
 
-		    int x = 1;
-		    int y = 2;
+			    GOval o = new GOval(2, 1); // width, height
 
-		    GOval o = new GOval(2, 1); // width, height
+			    println(racxa(x, 2, o));
+			    println(racxa(y, x / 2 + x, o));
 
-		    println(0 + 'h' - 'e');
+			  }
 
-		  }
+			  public int racxa(int x, int y, GOval o) {
+			    String s = "hello world";
+
+			    x = varesiRacxa(x / y, x + (int) o.getHeight(), s);
+			    o.scale(2);
+			    y = varesiRacxa(x, y + (int) o.getHeight(), s);
+
+			    return x + y;
+			  }
+
+			  public int varesiRacxa(double x, int y, String s) {
+			    int z = (int) x + s.charAt(0) - s.charAt(1);
+			    s = s.substring(1);
+
+			    return z + y;
+			  }
 
 }
