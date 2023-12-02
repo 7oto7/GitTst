4c03e72f0bb1637410dfd3ccc2bf59da7e6fe7f1
S <s>
2023-12-02 19:51:57 +0400

    {"types":"C","runs":3}

--- dedasSgeveci.java
+++ dedasSgeveci.java
@@ -1,3 +1,5 @@
+import java.awt.event.MouseEvent;
+
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
@@ -5,11 +7,22 @@ public class dedasSgeveci extends GraphicsProgram{
 	private GOval ball;
 	public void run(){
 		makeBall();
+		addMouseListeners();
 		
 	}
 	private void makeBall() {
 		ball = new GOval(40, 40);
 		ball.setFilled(true);
 		add(ball, 100, 100);
+	}
+	public void mouseclicked(MouseEvent e){
+		GOval obj = (GOval)getElementAt(e.getX(), e.getY());
+		if(obj != null){
+			while(true){
+			ball.move(0, 5);
+			pause(100);
+		}
+		}
+		
 	}
 }
