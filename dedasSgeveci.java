93ce896171f9b9df64cee059f7a48d4edd671d0b
S <s>
2023-12-02 19:49:19 +0400

    {"types":"C","runs":1}

--- dedasSgeveci.java
+++ dedasSgeveci.java
@@ -1,4 +1,15 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class dedasSgeveci {
-
+public class dedasSgeveci extends GraphicsProgram{
+	private GOval ball;
+	public void run(){
+		makeBall();
+		
+	}
+	private void makeBall() {
+		ball = new GOval(20, 20);
+		ball.setFilled(true);
+		add(ball, 100, 100);
+	}
 }
