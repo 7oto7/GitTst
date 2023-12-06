a1b1cb513995dabc9f4cfe6f8412dd9ec4ab8b30
S <s>
2023-12-06 17:48:31 +0400

    {"types":"C","runs":24}

--- problem55.java
+++ problem55.java
@@ -1,4 +1,10 @@
+//კონსოლიდან შეგყვავს n და შემდეგ n ცალი მთელი რიცხვი, გაარკვიეთ შეყვანილი რიცხვები
+//არის თუ არა ზრდადობით დალაგებული. თუკი არის დაბეჭდეთ “sorted” თუ არა მაშინ
+//დაბეჭდეთ “not sorted”.
+import acm.program.ConsoleProgram;
 
-public class problem55 {
-
+public class problem55 extends ConsoleProgram{
+	public void run(){
+		
+	}
 }
