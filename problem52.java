839382532ef14ac8f3959d6df7cb7b35781d88a3
S <s>
2023-12-06 16:06:00 +0400

    {"types":"C","runs":23}

--- problem52.java
+++ problem52.java
@@ -1,7 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import acm.program.ConsoleProgram;
 
 public class problem52 extends ConsoleProgram{
-	public void run(){
+//	private static final String FILENAME = "nonExistant file";
+	private static final String FILENAME = "src/Problem52.java";
+	public void run() {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(FILENAME));
+//			println("opened reader");
+			while(true) {
+				String newLine = reader.readLine();
+				if(newLine == null) {
+					break;
+				}
+				println(newLine);
+			}
+		
+			reader.close();
+			
+//			throw new FileNotFoundException();  // manually throw an exception
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			println("შეცდომა მოხდა, ფაილი ვერ მოიძებნა");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			println("სხვა შეცდომა მოხდა");
+		}
 		
 	}
 }
