370eff59fe1fdacd713ad1a8f3d7c982ea61ff27
S <s>
2023-12-07 18:54:40 +0400

    {"types":"C","runs":17}

--- problem56.java
+++ problem56.java
@@ -1,53 +1,45 @@
+
 //კონსოლიდან შეგყვავს n და შემდეგ n ცალი მთელი რიცხვი, დაალაგეთ რიცხვები
 //ზრდადობით და დაბეჭდეთ.
 import java.util.Arrays;
 
 import acm.program.ConsoleProgram;
 
-public class problem56 extends ConsoleProgram{
-	public void run(){
+public class problem56 extends ConsoleProgram {
+	public void run() {
 		int n = readInt("Enter n: ");
 		int[] nums = new int[n];
-		for(int i = 0; i < n; i++){
+		for (int i = 0; i < n; i++) {
 			nums[i] = readInt("Enter numbre: ");
 		}
-		String sorted = sort(nums);
-		println(sorted);
-		
+		sort(nums);
+
 	}
-	private String sort(int[] nums){
-		for(int i = 0; i < nums.length; i++){
-//			int j = findMinIndex(nums, i);
-//			swap(nums, i, j);
-			int a = nums[i];
-			int a2 = a;
-			for(int j = i + 1; j < nums.length; j++){
-				int b = nums[j];
-				if(b < a2){
-					a2 = b;
-					nums[i] = b;
-					nums[j] = a;
-				}
+
+	private void sort(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			int j = findMinIndex(nums, i);
+			swap(nums, i, j);
+
+			String str = Arrays.toString(nums);
+			println(str);
+		}
+	}
+
+	private void swap(int[] nums, int i, int j) {
+		int tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
+
+	}
+
+	private int findMinIndex(int[] nums, int i) {
+		int minIndex = i;
+		for (int j = i; j < nums.length; j++) {
+			if (nums[j] < nums[minIndex]) {
+				minIndex = j;
 			}
 		}
-		String str = Arrays.toString(nums);
-//		println(str);
-		return str;
+		return minIndex;
 	}
-//	private void swap(int[] nums, int i, int j) {
-//		int tmp = nums[i];
-//		nums[i] = nums[j];
-//		nums[j] = tmp;
-//				
-//	}
-	
-//	private int findMinIndex(int[] nums, int i) {
-//		int minIndex = i;
-//		for(int j = i; j < nums.length; j++){
-//			if(nums[j] < nums[minIndex]){
-//				minIndex = j;
-//			}
-//		}
-//		return minIndex;
-//	}
 }
