fea69cf67d70577550b3fba101e85373f3cdd7dc
S <s>
2023-12-07 18:45:46 +0400

    {"types":"C","runs":12}

--- problem56.java
+++ problem56.java
@@ -11,41 +11,41 @@ public class problem56 extends ConsoleProgram{
 		for(int i = 0; i < n; i++){
 			nums[i] = readInt("Enter numbre: ");
 		}
-		sort(nums);
-//		println(sorted);
+		String sorted = sort(nums);
+		println(sorted);
 		
 	}
-	private void sort(int[] nums){
+	private String sort(int[] nums){
 		for(int i = 0; i < nums.length; i++){
-			int j = findMinIndex(nums, i);
-			swap(nums, i, j);
-//			int a = nums[i];
-//			for(int j = i + 1; j < nums.length; j++){
-//				int b = nums[j];
-//				if(b < a){
-//					a = b;
-//				}
-//			}
-//			nums[i] = a;
+//			int j = findMinIndex(nums, i);
+//			swap(nums, i, j);
+			int a = nums[i];
+			for(int j = i + 1; j < nums.length; j++){
+				int b = nums[j];
+				if(b < a){
+					a = b;
+				}
+			}
+			nums[i] = a;
 		}
 		String str = Arrays.toString(nums);
-		println(str);
-//		return str;
+//		println(str);
+		return str;
 	}
-	private void swap(int[] nums, int i, int j) {
-		int tmp = nums[i];
-		nums[i] = nums[j];
-		nums[j] = tmp;
-				
-	}
+//	private void swap(int[] nums, int i, int j) {
+//		int tmp = nums[i];
+//		nums[i] = nums[j];
+//		nums[j] = tmp;
+//				
+//	}
 	
-	private int findMinIndex(int[] nums, int i) {
-		int minIndex = i;
-		for(int j = i; j < nums.length; j++){
-			if(nums[j] < nums[minIndex]){
-				minIndex = j;
-			}
-		}
-		return minIndex;
-	}
+//	private int findMinIndex(int[] nums, int i) {
+//		int minIndex = i;
+//		for(int j = i; j < nums.length; j++){
+//			if(nums[j] < nums[minIndex]){
+//				minIndex = j;
+//			}
+//		}
+//		return minIndex;
+//	}
 }
