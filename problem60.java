aca05cad4e1a3a4e1cfa817ac54365aa0d36ee52
S <s>
2023-12-12 17:41:55 +0400

    {"types":"C","runs":10}

--- problem60.java
+++ problem60.java
@@ -20,7 +20,7 @@ public class problem60 extends ConsoleProgram{
 	private String reversedList(ArrayList<Integer> list2) {
 		String reversed = "[]";
 		for(int i = list2.size() - 1; i >= 0; i--){
-			reversed = " " + list2.get(i);
+			reversed = reversed + " " + list2.get(i);
 		}
 		
 		return reversed;
