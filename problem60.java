ed5dcf144668f6f5bbdf0749dca066ee60ee095c
S <s>
2023-12-12 17:35:19 +0400

    {"types":"C","runs":3}

--- problem60.java
+++ problem60.java
@@ -4,7 +4,7 @@ import acm.program.ConsoleProgram;
 //კონსოლიდან შეგვყავს რიცხვები მანამ სანამ არ შეიყვანთ -1-ს, დაბეჭდეთ შეყვანილი
 //რიცხვები შებრუნებული მიმდევრობით.
 public class problem60 extends ConsoleProgram{
-	private ArrayList<Integer> list;
+	private ArrayList<Integer> list = new ArrayList<Integer>() ;
 	//private ArrayList<Integer> reversed;
 	public void run(){
 		while(true){
