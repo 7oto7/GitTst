337301ea2f3cfef15683e9aedf841919610d8b2a
S <s>
2023-12-15 13:10:12 +0400

    {"types":"C","runs":12}

--- problem61.java
+++ problem61.java
@@ -1,4 +1,14 @@
+import java.util.HashMap;
 
-public class problem61 {
+import acm.program.ConsoleProgram;
 
+public class problem61 extends ConsoleProgram{
+	HashMap <Integer, Integer> hashMpa = new HashMap<Integer, Integer>();
+	public void run(){
+		int m = readInt(), n = readInt();
+		int[] array = new int[n];
+		for(int i = 0; i < n; i++){
+			array[i] = readInt();
+		}
+	}
 }
