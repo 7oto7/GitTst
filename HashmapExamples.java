5c586d03df3927711207441302daf3f0a9dbec4d
S <s>
2023-12-15 12:47:53 +0400

    {"types":"C","runs":6}

--- HashmapExamples.java
+++ HashmapExamples.java
@@ -3,27 +3,58 @@ import java.util.HashMap;
 
 import acm.program.ConsoleProgram;
 
-public class HashmapExamples extends ConsoleProgram{
-	public void run(){
-		HashMap <String, Integer> hashMap = new HashMap <String, Integer>();
-		ArrayList <Integer> arrayList = new ArrayList <Integer>();
-		
-		if(!hashMap.containsKey("ABC")){
+public class HashmapExamples extends ConsoleProgram {
+	public void run() {
+		HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+		if (!hashMap.containsKey("ABC")) {
 			hashMap.put("ABC", 2000);
 		}
 		hashMap.putIfAbsent("ABC", 3000);
-		
-		hashMap.getOrDefault(key, 7); //null is nacvlad abrunebs default mnishvnelobas (7)
-		hashMap.get(key);
-		
+
+		//hashMap.getOrDefault(key, 7); // null is nacvlad abrunebs default
+										// mnishvnelobas (7)
+		//hashMap.get(key);
+
 		hashMap.put("ABC", 3000);
-		
+
 		println(hashMap.size());
 		println(hashMap);
 		println(hashMap.toString());
-		
-		for( String x : hashMap.keySet()){
+
+		for (String x : hashMap.keySet()) {
 			println(x + " " + hashMap.get(x));
 		}
+
+		
+		
+	
+		
+		HashMap<String, ArrayList> hashMap2 = new HashMap <String, ArrayList>();
+		hashMap2.put("ABC", arrayList);
+		
+		arrayList.add(10);
+		arrayList.add(100);
+		arrayList.add(1022);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
