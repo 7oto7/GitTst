231209c0a193c4251bb9ee649c726f4238f525dc
S <s>
2023-12-15 19:14:10 +0400

    {"types":"C","runs":9}

--- drawImageExample.java
+++ drawImageExample.java
@@ -7,7 +7,7 @@ import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class drawImageExample extends GraphicsProgram {
-	private static final int NUMBER_OF_PIXELS = 25000;
+	private static final int NUMBER_OF_PIXELS = 20000;
 	private static final int OVAL_SIZE = 5;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 
