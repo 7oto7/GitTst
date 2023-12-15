55ecb84e0f2352f5be6cec8d935e69d077a92033
S <s>
2023-12-15 19:36:16 +0400

    {"types":"C","runs":24}

--- drawTwoPictures.java
+++ drawTwoPictures.java
@@ -7,8 +7,8 @@ import acm.util.RandomGenerator;
 import java.awt.Color;
 
 public class drawTwoPictures extends GraphicsProgram {
-    private static final int NUMBER_OF_PIXELS = 65000;
-    private static final int OVAL_SIZE = 10;
+    private static final int NUMBER_OF_PIXELS = 70000;
+    private static final int OVAL_SIZE = 8;
     private RandomGenerator rgen = RandomGenerator.getInstance();
 
     public void run() {
