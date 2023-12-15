e24aac241db204ee038efa68da3d1deb42942b0b
S <s>
2023-12-15 19:12:52 +0400

    {"types":"C","runs":2}

--- drawImageExample.java
+++ drawImageExample.java
@@ -1,4 +1,39 @@
+import java.awt.Color;
 
-public class drawImageExample {
+import acm.graphics.GImage;
+import acm.graphics.GObject;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
+public class drawImageExample extends GraphicsProgram {
+	private static final int NUMBER_OF_PIXELS = 500;
+	private static final int OVAL_SIZE = 10;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	public void run() {
+		GImage image = new GImage("C:\\Users\\User\\Pictures\\download.jpg");
+		impress(image);
+	}
+
+	private void impress(GImage image) {
+		// TODO Auto-generated method stub
+		int[][] array = image.getPixelArray();
+
+		for (int i = 0; i < NUMBER_OF_PIXELS; i++) {
+			int r = rgen.nextInt(array.length);
+			int c = rgen.nextInt(array[0].length);
+			
+			Color color = new Color(array[r][c]);
+			add(getOval(c, r, color));
+		}
+	}
+
+	private GObject getOval(int c, int r, Color color) {
+		// TODO Auto-generated method stub
+		GOval o = new GOval(c - OVAL_SIZE / 2, r - OVAL_SIZE / 2, OVAL_SIZE, OVAL_SIZE);
+		o.setFilled(true);
+		o.setColor(color);
+		return o;
+	}
 }
