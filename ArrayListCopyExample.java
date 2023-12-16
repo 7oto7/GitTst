9fbebdb48aa0469135c5e90354f36011d6ed06df
S <s>
2023-12-16 19:47:09 +0400

    {"types":"C","runs":10}

--- ArrayListCopyExample.java
+++ ArrayListCopyExample.java
@@ -1,4 +1,22 @@
 
-public class ArrayListCopyExample {
+import java.util.ArrayList;
 
+public class ArrayListCopyExample {
+    public static void main(String[] args) {
+        // Create an ArrayList
+        ArrayList<Integer> originalList = new ArrayList<>();
+        originalList.add(1);
+        originalList.add(2);
+        originalList.add(3);
+
+        // Clone the ArrayList
+        ArrayList<Integer> copiedList = (ArrayList<Integer>) originalList.clone();
+
+        // Modify the original list
+        originalList.add(4);
+
+        // Print both lists
+        System.out.println("Original List: " + originalList);
+        System.out.println("Copied List: " + copiedList);
+    }
 }
