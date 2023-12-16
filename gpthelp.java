d8808971e059d345b022079db8c34d5f7621ce8f
S <s>
2023-12-16 18:48:05 +0400

    {"types":"C","runs":6}

--- gpthelp.java
+++ gpthelp.java
@@ -7,7 +7,7 @@ public class gpthelp extends JFrame {
 
     private static final int WIDTH = 1000;
     private static final int HEIGHT = 1000;
-    private static final int BALL_RADIUS = 400;
+    private static final int BALL_RADIUS = 500;
 
     public gpthelp() {
         setTitle("Circle Visualization");
