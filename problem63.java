7b0d6a4e76a26e95a873d1df39e7dda5615bbd9a
S <s>
2023-12-21 18:44:46 +0400

    {"types":"C","runs":11}

--- problem63.java
+++ problem63.java
@@ -27,7 +27,7 @@ public class problem63 extends ConsoleProgram{
 				friendLists.put(person, new ArrayList<String>());
 			}
 			
-			ArrayList<String> currFriends = friendLists.get(person);
+			ArrayList<String> currFriends = new ArrayList<>(friendLists.get(person));
 			currFriends.add(friend); // TODO add check for duplicates
 			
 			// we don't need to call put again
