d0277917a96e6f8a5abb0c18e611a91a46b953c0
S <s>
2023-12-26 17:58:32 +0400

    {"types":"C","runs":3}

--- problem68.java
+++ problem68.java
@@ -1,4 +1,24 @@
+import java.awt.event.ActionEvent;
 
-public class problem68 {
+import javax.swing.JTextField;
 
+import acm.program.GraphicsProgram;
+
+public class problem68 extends GraphicsProgram{
+	
+	private JTextField textField;
+	
+	public void init(){
+		textField = new JTextField(20);
+		add(textField, SOUTH);
+	}
+	
+	public void run(){
+		
+	}
+	
+	@override
+	public void actionPerformed(ActionEvent e){
+		
+	}
 }
