60c19fbb25e6c38d4cabf6de21af8f7126c4f1aa
S <s>
2023-12-27 17:49:00 +0400

    {"types":"C","runs":1}

--- problem62.java
+++ problem62.java
@@ -9,24 +9,41 @@ import acm.program.ConsoleProgram;
 public class problem62 extends ConsoleProgram{
 	private static final String SENTINEL = "";
 	public void run() {
-		Map<String, Integer> nameCounts = 
-				new HashMap<String, Integer>();
+//		Map<String, Integer> nameCounts = 
+//				new HashMap<String, Integer>();
+//		
+//		while(true) {
+//			String name = readLine("Enter name: ");
+//			if(name.equals(SENTINEL)) {
+//				break;
+//			}
+//			
+//			if(!nameCounts.containsKey(name)) {
+//				nameCounts.put(name, 0);
+//			}
+//			
+//			int newCount = nameCounts.get(name) + 1;
+//			nameCounts.put(name, newCount);
+//		}
+//		
+//		println(nameCounts);
 		
-		while(true) {
-			String name = readLine("Enter name: ");
-			if(name.equals(SENTINEL)) {
-				break;
-			}
-			
-			if(!nameCounts.containsKey(name)) {
-				nameCounts.put(name, 0);
-			}
-			
-			int newCount = nameCounts.get(name) + 1;
-			nameCounts.put(name, newCount);
-		}
-		
-		println(nameCounts);
+		 // create an HashMap
+	    HashMap<String, Integer> primeNumbers = new HashMap<>();
+
+	    // add mappings to HashMap
+	    primeNumbers.put("Two", 2);
+	    primeNumbers.put("Three", 3);
+	    System.out.println("Prime Numbers: " + primeNumbers);
+
+	    // create another HashMap
+	    HashMap<String, Integer> numbers = new HashMap<>();
+	    numbers.put("One", 1);
+	    numbers.put("Two", 22);
+
+	    // Add all mappings from primeNumbers to numbers
+	    numbers.putAll(primeNumbers);
+	    System.out.println("Numbers: " + numbers);
 		
 	}
 
