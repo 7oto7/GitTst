28b8fa35021aeaefaf02fc2f61989015ab6a3b64
S <s>
2023-12-27 16:06:34 +0400

    {"types":"C","runs":0}

--- problem62.java
+++ problem62.java
@@ -1,4 +1,33 @@
+//62. კონსოლიდან შეგვყავს სახელები, მანამ სანამ არ შევიყვანთ ცარიელ სტრინგს. 
+//დაბეჭდეთ სახელი და მის გასწვრივ რაოდენობა რამდენჯერ შევიყვანეთ ეს სახელი.
 
-public class problem62 {
+import java.util.HashMap;
+import java.util.Map;
+
+import acm.program.ConsoleProgram;
+
+public class problem62 extends ConsoleProgram{
+	private static final String SENTINEL = "";
+	public void run() {
+		Map<String, Integer> nameCounts = 
+				new HashMap<String, Integer>();
+		
+		while(true) {
+			String name = readLine("Enter name: ");
+			if(name.equals(SENTINEL)) {
+				break;
+			}
+			
+			if(!nameCounts.containsKey(name)) {
+				nameCounts.put(name, 0);
+			}
+			
+			int newCount = nameCounts.get(name) + 1;
+			nameCounts.put(name, newCount);
+		}
+		
+		println(nameCounts);
+		
+	}
 
 }
