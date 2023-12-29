48479e3486ffa761d830d460de5106b4038e43c3
S <s>
2023-12-29 19:02:28 +0400

    {"types":"C","runs":2}

--- Jcomponents.java
+++ Jcomponents.java
@@ -1,4 +1,26 @@
+import java.awt.event.ActionEvent;
 
-public class Jcomponents {
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
+import acm.program.ConsoleProgram;
+
+public class Jcomponents extends ConsoleProgram{
+	private JTextField field;
+
+	public void run(){
+		field = new JTextField(20);
+//		field.setText("pepi gcem");
+		add(field, SOUTH);
+		field.addActionListener(this);
+		
+		JButton button = new JButton("Ps me");
+		add(button, SOUTH);
+		addActionListeners();
+	}
+	public void actionPerformed(ActionEvent e) {
+		// You fill this in //
+		System.out.println(e.getActionCommand());
+		println(field.getText());
+	}
 }
