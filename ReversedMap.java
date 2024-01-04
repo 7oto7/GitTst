a5267d7d141ef42336df893a0cb9c17712a27a3d
S <s>
2024-01-04 23:00:28 +0400

    {"types":"C","runs":3}

--- ReversedMap.java
+++ ReversedMap.java
@@ -1,4 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
 
-public class ReversedMap {
+import acm.program.ConsoleProgram;
 
+public class ReversedMap extends ConsoleProgram{
+	public void run(){
+		Map<String, String> pets = new HashMap<String, String>();
+		pets.put("nika", "dog");
+		pets.put("ana", "cat");
+		pets.put("vaxo", "dog");
+		pets.put("bidzo", "zebra");
+		println(pets);
+	}
 }
